38c4837b72fcd10f279a6ea58118eb0afc6be8b0
Brent Carmer <bcarmer@galois.com>
2019-01-14 11:21:19 -0800

    update docs

--- docs/implementors/core/marker/trait.Copy.js
+++ docs/implementors/core/marker/trait.Copy.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["fancy_garbling"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"fancy_garbling/circuit/struct.Ref.html\" title=\"struct fancy_garbling::circuit::Ref\">Ref</a>",synthetic:false,types:["fancy_garbling::circuit::Ref"]},];
+implementors["fancy_garbling"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"fancy_garbling/circuit/struct.CircuitRef.html\" title=\"struct fancy_garbling::circuit::CircuitRef\">CircuitRef</a>",synthetic:false,types:["fancy_garbling::circuit::CircuitRef"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
