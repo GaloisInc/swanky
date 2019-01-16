877fe267ee11c9c0137ed4e87c369ff0de85da9b
Brent Carmer <bcarmer@galois.com>
2019-01-15 16:00:06 -0800

    update docs

---
+++ docs/source-files.js
@@ -0,0 +1,2 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex['fancy_garbling'] = {"name":"","dirs":[{"name":"garble","dirs":[],"files":["evaluator.rs","garbler.rs"]}],"files":["aes.rs","circuit.rs","dummy.rs","fancy.rs","garble.rs","informer.rs","lib.rs","util.rs","wire.rs"]};
