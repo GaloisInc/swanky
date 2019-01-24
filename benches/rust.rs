4b0edd4278c5c852852543b0aa1d2f62835b3c4c
Brent Carmer <bcarmer@galois.com>
2019-01-24 10:02:50 -0800

    more rust library benchmarks -- how expensive are RwLocks

---
+++ benches/rust.rs
@@ -0,0 +1,43 @@
+use criterion::{criterion_main, criterion_group, Criterion, black_box};
+
+fn bench_thread(c: &mut Criterion) {
+    c.bench_function("thread startup time", |b| {
+        b.iter(|| {
+            let h = std::thread::spawn(||());
+            black_box(h);
+        });
+    });
+}
+
+fn bench_mutex(c: &mut Criterion) {
+    c.bench_function("mutex lock time", |b| {
+        let m = std::sync::Mutex::new(());
+        b.iter(|| {
+            let v = m.lock().unwrap();
+            black_box(*v);
+        });
+    });
+}
+
+fn bench_rwlock_write(c: &mut Criterion) {
+    c.bench_function("rwlock write time", |b| {
+        let m = std::sync::RwLock::new(());
+        b.iter(|| {
+            let v = m.write().unwrap();
+            black_box(*v);
+        });
+    });
+}
+
+fn bench_rwlock_read(c: &mut Criterion) {
+    c.bench_function("rwlock read time", |b| {
+        let m = std::sync::RwLock::new(());
+        b.iter(|| {
+            let v = m.read().unwrap();
+            black_box(*v);
+        });
+    });
+}
+
+criterion_group!(benches, bench_thread, bench_mutex, bench_rwlock_write, bench_rwlock_read);
+criterion_main!(benches);
