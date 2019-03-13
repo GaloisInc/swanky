a234ff0412ec83194960ee2e9ac1cbd51f167bad
Brent Carmer <bcarmer@galois.com>
2019-03-13 14:06:56 -0700

    update docs

---
+++ docs/fancy_garbling/crt/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["Bundle","A collection of wires, useful for the garbled gadgets defined by `BundleGadgets`."]],"trait":[["BundleGadgets","Extension trait for `Fancy` providing advanced gadgets based on bundles of wires."]]});
