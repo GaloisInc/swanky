19667e71cabbd0805f9f5b84547b945935c74ae3
Alex J. Malozemoff <amaloz@galois.com>
2019-04-18 05:21:42 -0700

    add timing details to example

--- examples/psty.rs
+++ examples/psty.rs
@@ -7,53 +7,85 @@
 //! Private set intersection (PSTY) benchmarks using `criterion`.
 
 use popsicle::psty::{P1, P2};
+use scuttlebutt::comm::{TrackReader, TrackWriter};
 use scuttlebutt::AesRng;
 use std::io::{BufReader, BufWriter};
 use std::os::unix::net::UnixStream;
+use std::time::SystemTime;
 
-const SIZE: usize = 15;
+const NBYTES: usize = 15;
+const NTIMES: usize = 1 << 16;
 
 fn rand_vec(n: usize) -> Vec<u8> {
     (0..n).map(|_| rand::random::<u8>()).collect()
 }
 
 fn rand_vec_vec(size: usize) -> Vec<Vec<u8>> {
-    (0..size).map(|_| rand_vec(SIZE)).collect()
+    (0..size).map(|_| rand_vec(NBYTES)).collect()
 }
 
-fn psty(
-    inputs1: Vec<Vec<u8>>,
-    inputs2: Vec<Vec<u8>>,
-) -> (
-    Vec<ocelot::oprf::kkrt::Output>,
-    Vec<ocelot::oprf::kkrt::Output>,
-) {
+fn psty(inputs1: Vec<Vec<u8>>, inputs2: Vec<Vec<u8>>) {
     let (sender, receiver) = UnixStream::pair().unwrap();
-
+    let total = SystemTime::now();
     let handle = std::thread::spawn(move || {
         let mut rng = AesRng::new();
-        let mut reader = BufReader::new(sender.try_clone().unwrap());
-        let mut writer = BufWriter::new(sender);
+        let mut reader = TrackReader::new(BufReader::new(sender.try_clone().unwrap()));
+        let mut writer = TrackWriter::new(BufWriter::new(sender));
+        let start = SystemTime::now();
         let mut p1 = P1::init(&mut reader, &mut writer, &mut rng).unwrap();
+        println!(
+            "Sender init time: {} ms",
+            start.elapsed().unwrap().as_millis()
+        );
+        let start = SystemTime::now();
         p1.send(&mut reader, &mut writer, &inputs1, &mut rng)
-            .unwrap()
+            .unwrap();
+        println!(
+            "[{}] Send time: {} ms",
+            NTIMES,
+            start.elapsed().unwrap().as_millis()
+        );
+        println!(
+            "Sender communication (read): {:.2} Mb",
+            reader.kilobits() / 1000.0
+        );
+        println!(
+            "Sender communication (write): {:.2} Mb",
+            writer.kilobits() / 1000.0
+        );
     });
 
     let mut rng = AesRng::new();
-    let mut reader = BufReader::new(receiver.try_clone().unwrap());
-    let mut writer = BufWriter::new(receiver);
+    let mut reader = TrackReader::new(BufReader::new(receiver.try_clone().unwrap()));
+    let mut writer = TrackWriter::new(BufWriter::new(receiver));
+    let start = SystemTime::now();
     let mut p2 = P2::init(&mut reader, &mut writer, &mut rng).unwrap();
-    let p2_out = p2
+    println!(
+        "Receiver init time: {} ms",
+        start.elapsed().unwrap().as_millis()
+    );
+    let start = SystemTime::now();
+    let _ = p2
         .send(&mut reader, &mut writer, &inputs2, &mut rng)
         .unwrap();
-
-    let p1_out = handle.join().unwrap();
-
-    (p1_out, p2_out)
+    println!(
+        "[{}] Receiver time: {} ms",
+        NTIMES,
+        start.elapsed().unwrap().as_millis()
+    );
+    let _ = handle.join().unwrap();
+    println!(
+        "Receiver communication (read): {:.2} Mb",
+        reader.kilobits() / 1000.0
+    );
+    println!(
+        "Receiver communication (write): {:.2} Mb",
+        writer.kilobits() / 1000.0
+    );
+    println!("Total time: {} ms", total.elapsed().unwrap().as_millis());
 }
 
 fn main() {
-    let rs = rand_vec_vec(1 << 12);
-    let _v = psty(rs.clone(), rs.clone());
-    println!("done");
+    let rs = rand_vec_vec(NTIMES);
+    psty(rs.clone(), rs.clone());
 }
