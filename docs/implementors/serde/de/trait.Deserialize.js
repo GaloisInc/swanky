0ab81b277ec02b259113a71c53ece3cc4e938d92
Alex J. Malozemoff <amaloz@galois.com>
2019-04-29 09:34:59 -0700

    update documentation

---
+++ docs/implementors/serde/de/trait.Deserialize.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["scuttlebutt"] = [{text:"impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.90/serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"struct\" href=\"scuttlebutt/struct.Block.html\" title=\"struct scuttlebutt::Block\">Block</a>",synthetic:false,types:["scuttlebutt::block::Block"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
