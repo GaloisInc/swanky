9f745563627b16aa5b4a079a701664791c74ac30
Alex J. Malozemoff <amaloz@galois.com>
2019-04-29 09:31:19 -0700

    update docs

--- docs/implementors/std/io/trait.Write.js
+++ docs/implementors/std/io/trait.Write.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["scuttlebutt"] = [{text:"impl&lt;W:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a> for <a class=\"struct\" href=\"scuttlebutt/comm/struct.TrackWriter.html\" title=\"struct scuttlebutt::comm::TrackWriter\">TrackWriter</a>&lt;W&gt;",synthetic:false,types:["scuttlebutt::comm::TrackWriter"]},];
+implementors["scuttlebutt"] = [{text:"impl&lt;W:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a> for <a class=\"struct\" href=\"scuttlebutt/comm/struct.TrackWriter.html\" title=\"struct scuttlebutt::comm::TrackWriter\">TrackWriter</a>&lt;W&gt;",synthetic:false,types:["scuttlebutt::comm::TrackWriter"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
