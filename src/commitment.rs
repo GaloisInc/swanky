8ed809bf12489a527c1deb7a6dd8c37ae0bd49e2
Nichole Schimanski <nls@galois.com>
2019-05-02 09:25:17 -0700

    Add documentation to commitment

--- src/commitment.rs
+++ src/commitment.rs
@@ -1,16 +1,61 @@
-use sha2::{Sha256,Digest};
+// -*- mode: rust; -*-
+//
+// This file is part of `scuttlebutt`.
+// Copyright © 2019 Galois, Inc.
+// See LICENSE for licensing information.
 
+//! A trait defining a Commitment Scheme and an implementation in the random
+//! oracle model using SHA256.
+//!
+//! # Usage
+//! ```rust
+//! use crate::scuttlebutt::commitment::{Commitment, OracleCommitment};
+//!
+//! // define a seed
+//! let seed = b"seed".to_vec();
+//!
+//! // create a commitment object
+//! let mut commit = OracleCommitment::new(seed);
+//!
+//! // write input messages
+//! commit.input(b"hello ");
+//! commit.input(b"world");
+//!
+//! // finish commitment
+//! let commitment = commit.finish();
+//!
+//! // check a commitment
+//! let seed = b"seed".to_vec();
+//! let msg = b"hello world";
+//! let mut commit_ = OracleCommitment::new(seed);
+//! commit_.input(msg);
+//! let commitment_ = commit_.finish();
+//!
+//! assert!(OracleCommitment::check(&commitment,&commitment_));
+//! ```
+
+use sha2::{Digest, Sha256};
+
+/// Generic commitment scheme.
 pub trait Commitment {
+    /// The type used to initialize a commitment.
     type Seed;
+    /// The output type of the commitment.
     type Output;
 
+    /// A new commitment initialized with `seed`.
     fn new(seed: Self::Seed) -> Self;
+    /// A method to add data to the commitment.
     fn input(&mut self, input: &[u8]) -> ();
+    /// Complete the commitment.
     fn finish(self) -> Self::Output;
+    /// Check if two commitments are equal.
     fn check(comm1: &Self::Output, comm2: &Self::Output) -> bool;
 }
 
-pub struct OracleCommitment  {
+/// A commitment in the random oracle model using SHA256.
+pub struct OracleCommitment {
+    /// The seed used to initialize the commitment.
     pub seed: Vec<u8>,
     commit: Sha256,
 }
@@ -39,7 +84,6 @@ impl Commitment for OracleCommitment {
     fn check(comm1: &[u8; 32], comm2: &[u8; 32]) -> bool {
         comm1 == comm2
     }
-
 }
 
 #[cfg(test)]
@@ -50,13 +94,37 @@ mod tests {
     fn commit_hello_world() {
         let mut commit = OracleCommitment::new(b"hello".to_vec());
         commit.input(b" world");
-        
+
         let result = commit.finish();
-        println!("{:?}", result);
-        
-        assert_eq!(hex::encode(result), "b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9");
+
+        assert_eq!(
+            hex::encode(result),
+            "b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9"
+        );
     }
 
+    #[test]
+    fn commit_check() {
+        // define a seed
+        let seed = b"seed".to_vec();
+
+        // create a commitment object
+        let mut commit = OracleCommitment::new(seed);
+
+        // write input messages
+        commit.input(b"hello ");
+        commit.input(b"world");
+
+        // finish commitment
+        let commitment = commit.finish();
+
+        // check a commitment
+        let seed = b"seed".to_vec();
+        let msg = b"hello world";
+        let mut commit_ = OracleCommitment::new(seed);
+        commit_.input(msg);
+        let commitment_ = commit_.finish();
+
+        assert!(OracleCommitment::check(&commitment, &commitment_));
+    }
 }
-
-
