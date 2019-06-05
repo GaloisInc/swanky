16dfc47d7d037325d51c372062b4a9ffe9e16bcc
Alex J. Malozemoff <amaloz@galois.com>
2019-06-05 09:32:09 -0700

    documentation

---
+++ docs/implementors/core/fmt/trait.Display.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["twopac"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"twopac/enum.Error.html\" title=\"enum twopac::Error\">Error</a>",synthetic:false,types:["twopac::errors::Error"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
