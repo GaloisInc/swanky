bcc1349dc8b35c097e72bf2b65792881dd6be218
Alex J. Malozemoff <amaloz@galois.com>
2019-06-04 17:10:51 -0700

    update docs

--- docs/implementors/serde/ser/trait.Serialize.js
+++ docs/implementors/serde/ser/trait.Serialize.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["scuttlebutt"] = [{text:"impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.91/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"scuttlebutt/struct.Block.html\" title=\"struct scuttlebutt::Block\">Block</a>",synthetic:false,types:["scuttlebutt::block::Block"]},];
+implementors["scuttlebutt"] = [{text:"impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.92/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"scuttlebutt/struct.Block.html\" title=\"struct scuttlebutt::Block\">Block</a>",synthetic:false,types:["scuttlebutt::block::Block"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
