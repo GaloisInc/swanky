9c042bd5a7756cb59cd458e69e64496810a2eb21
Brent Carmer <bcarmer@galois.com>
2019-06-12 15:10:29 -0700

    formatting

--- src/psi/mod.rs
+++ src/psi/mod.rs
@@ -4,17 +4,21 @@
 // Copyright © 2019 Galois, Inc.
 // See LICENSE for licensing information.
 
+pub mod psz;
+
 #[cfg(feature = "unstable")]
 pub mod psty;
-pub mod psz;
 
 /// Private set intersection sender.
 pub type PsiSender = psz::Sender;
+
 /// Private set intersection receiver.
 pub type PsiReceiver = psz::Receiver;
+
 #[cfg(feature = "unstable")]
 /// Extended private set intersection sender.
 pub type ExtendPsiSender = psty::Sender;
+
 #[cfg(feature = "unstable")]
 /// Extended private set intersection sender.
 pub type ExtendPsiReceiver = psty::Receiver;
