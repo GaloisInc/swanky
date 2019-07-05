6a39a0adb10e5cea2cb744bee66c8b4a572a08d4
Brent Carmer <bcarmer@galois.com>
2019-07-05 14:41:36 -0700

    wip

--- src/psi/psz.rs
+++ src/psi/psz.rs
@@ -12,11 +12,13 @@
 
 use crate::cuckoo::{compute_masksize, CuckooHash};
 use crate::{utils, Error};
+use digest::Digest;
+use itertools::Itertools;
 use ocelot::oprf::{self, Receiver as OprfReceiver, Sender as OprfSender};
 use rand::seq::SliceRandom;
 use rand::{CryptoRng, Rng, RngCore};
-use scuttlebutt::utils as scutils;
 use scuttlebutt::{cointoss, AbstractChannel, Block, Block512, SemiHonest};
+use sha2::Sha256;
 use std::collections::HashSet;
 
 const NHASHES: usize = 3;
@@ -63,11 +65,11 @@ impl Sender {
         if payload_len > 0 {
             assert!(payloads.iter().all(|p| p.len() == payload_len));
             assert_eq!(payloads.len(), inputs.len());
+            channel.write_usize(payload_len)?;
         }
-        channel.write_usize(payload_len)?;
 
         let key = cointoss::send(channel, &[rng.gen()])?[0];
-        let mut inputs = utils::compress_and_hash_inputs(inputs, key);
+        let inputs = utils::compress_and_hash_inputs(inputs, key);
         let masksize = compute_masksize(inputs.len())?;
         let nbins = channel.read_usize()?;
         let seeds = self.oprf.send(channel, nbins, rng)?;
@@ -75,20 +77,43 @@ impl Sender {
         // For each hash function `hᵢ`, construct set `Hᵢ = {F(k_{hᵢ(x)}, x ||
         // i) | x ∈ X)}`, randomly permute it, and send it to the receiver.
         let mut encoded = Block512::default();
+        let mut indices = (0..inputs.len()).collect_vec();
         for i in 0..NHASHES {
-            inputs.shuffle(rng);
+            // shuffle the indices in order to send out of order
+            indices.shuffle(rng);
+
             let hidx = Block::from(i as u128);
-            for input in &inputs {
+            for &j in &indices {
                 // Compute `bin := hᵢ(x)`.
-                let bin = CuckooHash::bin(*input, i, nbins);
+                let bin = CuckooHash::bin(inputs[j], i, nbins);
+
                 // Compute `F(k_{hᵢ(x)}, x || i)` and chop off extra bytes.
-                self.oprf.encode(*input ^ hidx, &mut encoded);
-                scutils::xor_inplace_n(
-                    &mut encoded.prefix_mut(masksize),
-                    &seeds[bin].prefix(masksize),
-                    masksize,
-                );
-                channel.write_bytes(&encoded.prefix(masksize))?;
+                self.oprf.encode(inputs[j] ^ hidx, &mut encoded);
+                encoded ^= seeds[bin];
+
+                // If payloads are not present, use the original protocol.
+                // Otherwise, use the new protocol and send payloads encrypted under
+                // F(...), tagged by H(F(...)).
+                if payload_len == 0 {
+                    channel.write_bytes(&encoded.prefix(masksize))?;
+                } else {
+                    // compute and send tag
+                    let tag: [u8; 32] = Sha256::digest(encoded.prefix(masksize)).into();
+
+                    // encrypt payload
+                    let iv: [u8; 16] = rng.gen();
+                    let key = encoded.prefix(16);
+                    let ct = openssl::symm::encrypt(
+                        openssl::symm::Cipher::aes_128_ctr(),
+                        &key,
+                        Some(&iv),
+                        &payloads[j],
+                    )?;
+
+                    channel.write_bytes(&tag)?;
+                    channel.write_bytes(&iv)?;
+                    channel.write_bytes(&ct)?;
+                }
             }
         }
         channel.flush()?;
@@ -113,13 +138,39 @@ impl Receiver {
         inputs: &[Vec<u8>],
         rng: &mut RNG,
     ) -> Result<Vec<Vec<u8>>, Error> {
-        let res = self.receive_payloads(channel, inputs, rng)?;
-        Ok(res.into_iter().map(|(x, _)| x).collect())
+        let n = inputs.len();
+        let masksize = compute_masksize(n)?;
+
+        let (tbl, outputs) = self.perform_oprfs(channel, inputs, rng)?;
+
+        // Receive all the sets from the sender.
+        let mut hs = vec![HashSet::with_capacity(n); NHASHES];
+        for h in hs.iter_mut() {
+            for _ in 0..n {
+                let buf = channel.read_vec(masksize)?;
+                h.insert(buf);
+            }
+        }
+
+        // Iterate through each input/output pair and see whether it exists in
+        // the appropriate set.
+        let mut intersection = Vec::with_capacity(n);
+        for (opt_item, output) in tbl.items.iter().zip(outputs.into_iter()) {
+            if let Some(item) = opt_item {
+                let prefix = output.prefix(masksize);
+                if hs[item.hash_index].contains(prefix) {
+                    let val = inputs[item.input_index].clone();
+                    intersection.push(val);
+                }
+            }
+        }
+
+        Ok(intersection)
     }
 
     /// Run the PSI protocol over `inputs`, receiving a vector of tuples consisting of
     /// the intersection items and associated payloads.
-    fn receive_payloads<C: AbstractChannel, RNG: CryptoRng + RngCore>(
+    pub fn receive_payloads<C: AbstractChannel, RNG: CryptoRng + RngCore>(
         &mut self,
         channel: &mut C,
         inputs: &[Vec<u8>],
@@ -131,13 +182,28 @@ impl Receiver {
         )>,
         Error,
     > {
-        let n = inputs.len();
         let payload_len = channel.read_usize()?;
+        let (tbl, outputs) = self.perform_oprfs(channel, inputs, rng)?;
+        unimplemented!()
+    }
+
+    // Helper to do computation common to both receive and receive_payloads
+    fn perform_oprfs<C: AbstractChannel, RNG: CryptoRng + RngCore>(
+        &mut self,
+        channel: &mut C,
+        inputs: &[Vec<u8>],
+        rng: &mut RNG,
+    ) -> Result<
+        (
+            CuckooHash,    // Cuckoo Table
+            Vec<Block512>, // OPRF outputs
+        ),
+        Error,
+    > {
         let key = cointoss::receive(channel, &[rng.gen()])?[0];
 
         let tbl = CuckooHash::new(&utils::compress_and_hash_inputs(inputs, key), NHASHES)?;
         let nbins = tbl.nbins;
-        let masksize = compute_masksize(n)?;
 
         // Send cuckoo hash info to sender.
         channel.write_usize(nbins)?;
@@ -157,31 +223,9 @@ impl Receiver {
             })
             .collect::<Vec<Block>>();
 
-        let outputs = self.oprf.receive(channel, &oprf_inputs, rng)?;
+        let oprf_outputs = self.oprf.receive(channel, &oprf_inputs, rng)?;
 
-        // Receive all the sets from the sender.
-        let mut hs = vec![HashSet::with_capacity(n); NHASHES];
-        for h in hs.iter_mut() {
-            for _ in 0..n {
-                let buf = channel.read_vec(masksize)?;
-                h.insert(buf);
-            }
-        }
-
-        // Iterate through each input/output pair and see whether it exists in
-        // the appropriate set.
-        let mut intersection = Vec::with_capacity(n);
-        for (opt_item, output) in tbl.items.iter().zip(outputs.into_iter()) {
-            if let Some(item) = opt_item {
-                let prefix = output.prefix(masksize);
-                if hs[item.hash_index].contains(prefix) {
-                    let val = inputs[item.input_index].clone();
-                    intersection.push((val, Vec::new()));
-                }
-            }
-        }
-
-        Ok(intersection)
+        Ok((tbl, oprf_outputs))
     }
 }
 
@@ -196,14 +240,14 @@ mod tests {
     use std::io::{BufReader, BufWriter};
     use std::os::unix::net::UnixStream;
 
-    const SIZE: usize = 16;
-    const NTIMES: usize = 1 << 4;
+    const ITEM_SIZE: usize = 16;
+    const SET_SIZE: usize = 1 << 4;
 
     #[test]
     fn test_psi() {
         let mut rng = AesRng::new();
         let (sender, receiver) = UnixStream::pair().unwrap();
-        let sender_inputs = rand_vec_vec(NTIMES, SIZE, &mut rng);
+        let sender_inputs = rand_vec_vec(SET_SIZE, ITEM_SIZE, &mut rng);
         let receiver_inputs = sender_inputs.clone();
         let handle = std::thread::spawn(move || {
             let mut rng = AesRng::new();
@@ -222,6 +266,36 @@ mod tests {
             .receive(&mut channel, &receiver_inputs, &mut rng)
             .unwrap();
         handle.join().unwrap();
-        assert_eq!(intersection.len(), NTIMES);
+        assert_eq!(intersection.len(), SET_SIZE);
+    }
+
+    #[test]
+    fn test_payloads() {
+        let mut rng = AesRng::new();
+        let (sender, receiver) = UnixStream::pair().unwrap();
+        let sender_inputs = rand_vec_vec(SET_SIZE, ITEM_SIZE, &mut rng);
+        let receiver_inputs = sender_inputs.clone();
+        let payloads = rand_vec_vec(SET_SIZE, 32, &mut rng);
+
+        let handle = std::thread::spawn(move || {
+            let mut rng = AesRng::new();
+            let reader = BufReader::new(sender.try_clone().unwrap());
+            let writer = BufWriter::new(sender);
+            let mut channel = Channel::new(reader, writer);
+            let mut psi = Sender::init(&mut channel, &mut rng).unwrap();
+            psi.send_payloads(&mut channel, &sender_inputs, &payloads, &mut rng)
+                .unwrap();
+        });
+
+        let mut rng = AesRng::new();
+        let reader = BufReader::new(receiver.try_clone().unwrap());
+        let writer = BufWriter::new(receiver);
+        let mut channel = Channel::new(reader, writer);
+        let mut psi = Receiver::init(&mut channel, &mut rng).unwrap();
+        let payloads = psi
+            .receive_payloads(&mut channel, &receiver_inputs, &mut rng)
+            .unwrap();
+        handle.join().unwrap();
+        assert_eq!(payloads.len(), SET_SIZE);
     }
 }
