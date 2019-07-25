bf642b678371f1198f7cab0a9995c6afe0c83940
Brent Carmer <bcarmer@galois.com>
2019-07-25 12:01:11 -0700

    script for generating data for secretbrother

---
+++ popsicle/scripts/secretbrother-gen-data.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+
+use v5.30;
+
+my $party = $ARGV[0];
+my $nrecords = $ARGV[1];
+
+for (my $i=0; $i < $nrecords; $i++) {
+    printf "%03d-%02d-%04d", rand(1000), rand(100), rand(10000);
+
+    if ($party eq "receiver") {
+        say;
+        next;
+    }
+
+    print ", ";
+    print join ", ", rand(1000), rand(1000), rand(1000);
+    print ", ";
+    my @letters = ('a'..'z', 'A'..'Z', ' '); 
+    print $letters[rand(@letters)] for 0..24;
+    say;
+}
