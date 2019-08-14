0513c5509bb742532f5b59a5214e7dcdd9b58910
Alex J. Malozemoff <amaloz@galois.com>
2019-08-14 07:47:20 -0700

    get things building in nightly

--- fancy-garbling/src/twopac/mod.rs
+++ fancy-garbling/src/twopac/mod.rs
@@ -4,12 +4,6 @@
 // Copyright © 2019 Galois, Inc.
 // See LICENSE for licensing information.
 
-#![allow(clippy::many_single_char_names)]
-#![cfg_attr(feature = "nightly", feature(test))]
-#![cfg_attr(feature = "nightly", feature(external_doc))]
-#![cfg_attr(feature = "nightly", doc(include = "../README.md"))]
-#![cfg_attr(feature = "nightly", deny(missing_docs))]
-
 //! Implementations of two-party secure computation.
 
 pub mod semihonest;
