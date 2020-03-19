7afda95f60e8fd0abc4292d3dec9272e3226629b
Alex J. Malozemoff <amaloz@galois.com>
2020-03-19 16:53:52 -0700

    update docs

--- docs/implementors/rand_core/trait.SeedableRng.js
+++ docs/implementors/rand_core/trait.SeedableRng.js
@@ -1,9 +1,3 @@
 (function() {var implementors = {};
-implementors["scuttlebutt"] = [{text:"impl <a class=\"trait\" href=\"https://rust-random.github.io/rand/rand_core/trait.SeedableRng.html\" title=\"trait rand_core::SeedableRng\">SeedableRng</a> for <a class=\"struct\" href=\"scuttlebutt/struct.AesRng.html\" title=\"struct scuttlebutt::AesRng\">AesRng</a>",synthetic:false,types:["scuttlebutt::rand_aes::AesRng"]},];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        })()
+implementors["scuttlebutt"] = [{"text":"impl <a class=\"trait\" href=\"https://rust-random.github.io/rand/rand_core/trait.SeedableRng.html\" title=\"trait rand_core::SeedableRng\">SeedableRng</a> for <a class=\"struct\" href=\"scuttlebutt/struct.AesRng.html\" title=\"struct scuttlebutt::AesRng\">AesRng</a>","synthetic":false,"types":["scuttlebutt::rand_aes::AesRng"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
