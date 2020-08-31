3db58ca27e6304d7a5c95b448f90c1b255c93f9e
Alex J. Malozemoff <amaloz@galois.com>
2020-08-31 13:27:23 -0700

    edits to svole/copee stuff

--- ocelot/src/svole/copee.rs
+++ ocelot/src/svole/copee.rs
@@ -4,9 +4,8 @@
 // Copyright © 2020 Galois, Inc.
 // See LICENSE for licensing information.
 
-//! Correlated Oblivious Product Evaluation with errors (COPEe)
-//!
-//! This module provides implementations of COPEe Traits.
+//! Implementation of the Weng-Yang-Katz-Wang COPEe protocol (cf.
+//! <https://eprint.iacr.org/2020/925>).
 
 use crate::{
     errors::Error,
@@ -15,15 +14,9 @@ use crate::{
 };
 use digest::generic_array::typenum::Unsigned;
 use generic_array::GenericArray;
-use rand::SeedableRng;
+use rand::{CryptoRng, Rng, SeedableRng};
 use scuttlebutt::{
-    field::FiniteField as FF,
-    utils::unpack_bits,
-    AbstractChannel,
-    Aes128,
-    AesRng,
-    Block,
-    Malicious,
+    field::FiniteField as FF, utils::unpack_bits, AbstractChannel, Aes128, AesRng, Block, Malicious,
 };
 use std::{
     marker::PhantomData,
@@ -31,7 +24,7 @@ use std::{
 };
 use subtle::{Choice, ConditionallySelectable};
 
-/// A COPEe Sender.
+/// COPEe sender.
 #[derive(Clone)]
 pub struct Sender<ROT: ROTSender + Malicious, FE: FF> {
     _fe: PhantomData<FE>,
@@ -40,7 +33,7 @@ pub struct Sender<ROT: ROTSender + Malicious, FE: FF> {
     nbits: usize,
 }
 
-/// A COPEe Receiver.
+/// COPEe receiver.
 #[derive(Clone)]
 pub struct Receiver<ROT: ROTReceiver + Malicious, FE: FF> {
     _fe: PhantomData<FE>,
@@ -52,34 +45,17 @@ pub struct Receiver<ROT: ROTReceiver + Malicious, FE: FF> {
 
 /// Pack FE into a Vec<FE> whose entries are either `FE::PrimeField::zero()` or
 /// `FE::PrimeField::one()`.
-#[inline]
-pub fn pack_bits_fps<FE: FF>(x: FE) -> Vec<FE::PrimeField> {
+fn pack_bits_fps<FE: FF>(x: FE) -> Vec<FE::PrimeField> {
     let mut res = Vec::default();
     let r0 = FE::PrimeField::zero();
     let r1 = FE::PrimeField::one();
     let bv = unpack_bits(x.to_bytes().as_slice(), FE::ByteReprLen::to_usize() * 8);
-    for i in 0..bv.len() {
-        let choice = Choice::from(bv[i] as u8);
+    for b in bv.iter() {
+        let choice = Choice::from(*b as u8);
         let value = FE::PrimeField::conditional_select(&r0, &r1, choice);
         res.push(value);
     }
     res
-}
-
-/// Unpack Vec<FE> into a FE.
-#[inline]
-pub fn unpack_bits_fps<FE: FF>(x: Vec<FE::PrimeField>) -> FE {
-    let mut sum = FE::zero();
-    let nbits = FE::ByteReprLen::to_usize() * 8;
-    let mut two = FE::one();
-    two.add_assign(FE::one());
-    for i in 0..nbits {
-        let two_ = two.clone();
-        let mut powr = two_.pow(i as u128);
-        powr.mul_assign(to_fpr(x[i as usize]));
-        sum.add_assign(powr);
-    }
-    sum
 }
 
 /// Convert `Fp` to `F(p^r)`
@@ -90,12 +66,8 @@ pub fn to_fpr<FE: FF>(x: FE::PrimeField) -> FE {
     FE::from_polynomial_coefficients(g_arr)
 }
 
-/// Convert `F(p^r)` to `F(p)`
-pub fn to_fp<FE: FF>(x: FE) -> FE::PrimeField {
-    FE::to_polynomial_coefficients(&x)[0]
-}
 /// Compute dot product `<g,x>`
-pub fn g_dotprod<FE: FF>(x: Vec<FE::PrimeField>) -> FE {
+fn g_dotprod<FE: FF>(x: Vec<FE::PrimeField>) -> FE {
     let r = FE::PolynomialFormNumCoefficients::to_usize();
     let nbits = FE::ByteReprLen::to_usize() * 8;
     assert_eq!(x.len(), r * nbits);
@@ -119,12 +91,20 @@ pub fn g_dotprod<FE: FF>(x: Vec<FE::PrimeField>) -> FE {
     to_fpr(res)
 }
 
+fn prf<FE: FF>(key: Block, pt: Block) -> FE::PrimeField {
+    let aes = Aes128::new(key);
+    let seed = aes.encrypt(pt);
+    let mut rng = AesRng::from_seed(seed);
+    FE::PrimeField::random(&mut rng)
+}
+
 /// Implement CopeeSender for Sender type
 impl<ROT: ROTSender<Msg = Block> + Malicious, FE: FF> CopeeSender for Sender<ROT, FE> {
     type Msg = FE;
-    fn init<C: AbstractChannel>(channel: &mut C) -> Result<Self, Error> {
-        let seed = rand::random::<Block>();
-        let mut rng = AesRng::from_seed(seed);
+    fn init<C: AbstractChannel, RNG: CryptoRng + Rng>(
+        channel: &mut C,
+        mut rng: &mut RNG,
+    ) -> Result<Self, Error> {
         let mut ot = ROT::init(channel, &mut rng).unwrap();
         let nbytes = FE::ByteReprLen::to_usize();
         let samples = ot
@@ -145,30 +125,24 @@ impl<ROT: ROTSender<Msg = Block> + Malicious, FE: FF> CopeeSender for Sender<ROT
     fn send<C: AbstractChannel>(
         &mut self,
         channel: &mut C,
-        input: Vec<FE::PrimeField>,
+        input: &[FE::PrimeField],
     ) -> Result<Vec<FE>, Error> {
         let mut w = Vec::default();
-        for j in 0..input.len() {
+        for (j, u) in input.iter().enumerate() {
             let mut wv = Vec::default();
             for i in 0..self.nbits * FE::PolynomialFormNumCoefficients::to_usize() {
                 // Aes encryption as a PRF
                 let pt = Block::from(j as u128);
-                let key0 = Block::from(self.sv[i].0);
-                let cipher0 = Aes128::new(key0);
-                let seed0 = cipher0.encrypt(pt);
-                let mut rng0 = AesRng::from_seed(seed0);
-                let mut w0 = FE::PrimeField::random(&mut rng0);
-                let key1 = Block::from(self.sv[i].1);
-                let cipher1 = Aes128::new(key1);
-                let seed1 = cipher1.encrypt(pt);
-                let mut rng1 = AesRng::from_seed(seed1);
-                let w1 = FE::PrimeField::random(&mut rng1);
+                let mut w0 = prf::<FE>(self.sv[i].0, pt);
+                let w1 = prf::<FE>(self.sv[i].1, pt);
                 wv.push((w0, w1));
-                (w0.sub_assign(w1));
-                w0.sub_assign(input[j]);
-                channel.write_bytes(w0.to_bytes().as_slice())?;
+                w0.sub_assign(w1);
+                w0.sub_assign(*u);
+                channel.write_bytes(&w0.to_bytes())?;
             }
             channel.flush()?;
+            // TODO: would be better to compute this "on-the-fly" versus
+            // collecting things into a vector and doing it here.
             w.push(g_dotprod(wv.into_iter().map(|x| x.0).collect()));
         }
         Ok(w)
@@ -178,24 +152,25 @@ impl<ROT: ROTSender<Msg = Block> + Malicious, FE: FF> CopeeSender for Sender<ROT
 /// Implement CopeeReceiver for Receiver type.
 impl<ROT: ROTReceiver<Msg = Block> + Malicious, FE: FF> CopeeReceiver for Receiver<ROT, FE> {
     type Msg = FE;
-    fn init<C: AbstractChannel>(channel: &mut C) -> Result<Self, Error> {
+    fn init<C: AbstractChannel, RNG: CryptoRng + Rng>(
+        channel: &mut C,
+        mut rng: &mut RNG,
+    ) -> Result<Self, Error> {
         let nbytes = FE::ByteReprLen::to_usize();
-        let seed = rand::random::<Block>();
-        let mut rng = AesRng::from_seed(seed);
         let mut ot = ROT::init(channel, &mut rng).unwrap();
         let delta = FE::random(&mut rng);
-        let deltab = unpack_bits(delta.to_bytes().as_slice(), nbytes * 8);
-        let ots = ot.receive_random(channel, &deltab, &mut rng).unwrap();
+        let choices = unpack_bits(delta.to_bytes().as_slice(), nbytes * 8); // XXX: shouldn't this be * FE::PolynomialFormNumCoefficients::to_usize()?
+        let mv = ot.receive_random(channel, &choices, &mut rng).unwrap();
         Ok(Self {
             _fe: PhantomData::<FE>,
             _ot: PhantomData::<ROT>,
             delta,
-            choices: deltab,
-            mv: ots,
+            choices,
+            mv,
         })
     }
 
-    fn get_delta(&self) -> FE {
+    fn delta(&self) -> FE {
         self.delta
     }
 
@@ -205,24 +180,24 @@ impl<ROT: ROTReceiver<Msg = Block> + Malicious, FE: FF> CopeeReceiver for Receiv
         len: usize,
     ) -> Result<Vec<FE>, Error> {
         let mut output = Vec::default();
+        // XXX: shouldn't this be `self.choices`?
         let delta_fp = pack_bits_fps(self.delta);
         let nbytes = FE::ByteReprLen::to_usize();
         for j in 0..len {
             let mut v = Vec::default();
-            for i in 0..nbytes * 8 * FE::PolynomialFormNumCoefficients::to_usize() {
+            for (k, delta) in self.mv.iter().zip(delta_fp.iter()) {
                 let pt = Block::from(j as u128);
-                let key = Block::from(self.mv[i]);
-                let cipher = Aes128::new(key);
-                let seed = cipher.encrypt(pt);
-                let mut rng = AesRng::from_seed(seed);
-                let mut w_delta = FE::PrimeField::random(&mut rng);
+                let mut w_delta = prf::<FE>(*k, pt);
+                // XXX: better to add a capability to channel for reading in field elements
                 let mut data = vec![0u8; nbytes];
                 channel.read_bytes(&mut data)?;
                 let mut tau = FE::PrimeField::from_bytes(GenericArray::from_slice(&data)).unwrap();
-                tau.mul_assign(delta_fp[i]);
+                tau.mul_assign(*delta);
                 w_delta.add_assign(tau);
                 v.push(w_delta);
             }
+            // TODO: would be better to compute this "on-the-fly" versus
+            // collecting things into a vector and doing it here.
             output.push(g_dotprod(v));
         }
         Ok(output)
@@ -232,14 +207,33 @@ impl<ROT: ROTReceiver<Msg = Block> + Malicious, FE: FF> CopeeReceiver for Receiv
 #[cfg(test)]
 mod tests {
     use super::*;
-    use rand::SeedableRng;
     use scuttlebutt::{
         field::{FiniteField as FF, Fp},
         AesRng,
     };
+
+    /// Unpack Vec<FE> into a FE.
+    fn unpack_bits_fps<FE: FF>(x: Vec<FE::PrimeField>) -> FE {
+        let mut sum = FE::zero();
+        let nbits = FE::ByteReprLen::to_usize() * 8;
+        let mut two = FE::one();
+        two.add_assign(FE::one());
+        for i in 0..nbits {
+            let two_ = two.clone();
+            let mut powr = two_.pow(i as u128);
+            powr.mul_assign(to_fpr(x[i as usize]));
+            sum.add_assign(powr);
+        }
+        sum
+    }
+
+    /// Convert `F(p^r)` to `F(p)`
+    fn to_fp<FE: FF>(x: FE) -> FE::PrimeField {
+        FE::to_polynomial_coefficients(&x)[0]
+    }
+
     fn bit_composition<FE: FF>() {
-        let seed = rand::random::<Block>();
-        let mut rng = AesRng::from_seed(seed);
+        let mut rng = AesRng::new();
         let x = FE::random(&mut rng);
         let bv = pack_bits_fps(x);
         assert_eq!(unpack_bits_fps::<FE>(bv), x);
@@ -250,24 +244,23 @@ mod tests {
         bit_composition::<Fp>();
     }
 
+    fn g_dotproduct<FE: FF>() {
+        let mut rng = AesRng::new();
+        let x = Fp::random(&mut rng);
+        assert_eq!(g_dotprod::<Fp>(pack_bits_fps(x)), x);
+    }
+
     #[test]
     fn test_g_dotproduct() {
         g_dotproduct::<Fp>();
     }
 
-    fn g_dotproduct<FE: FF>() {
-        let seed = rand::random::<Block>();
-        let mut rng = AesRng::from_seed(seed);
-        let x = FE::random(&mut rng);
-        assert_eq!(g_dotprod::<FE>(pack_bits_fps(x)), x);
-    }
-
     fn to_type_fpr<FE: FF>() {
-        let seed = rand::random::<Block>();
-        let mut rng = AesRng::from_seed(seed);
+        let mut rng = AesRng::new();
         let x = FE::random(&mut rng);
         assert_eq!(to_fpr::<FE>(to_fp(x)), x);
     }
+
     #[test]
     fn test_to_fpr() {
         to_type_fpr::<Fp>();
