cd62307383f564c09fccd0fa0eebfe40c19a2cd2
Ajay Kumar Eeralla <aeeralla@galois.com>
2020-09-15 10:26:06 -0700

    fixed benchmarks for svole

--- ocelot/benches/base_vole.rs
+++ ocelot/benches/base_vole.rs
@@ -8,24 +8,20 @@
 
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use ocelot::{
-    ot::{KosReceiver, KosSender, RandomReceiver as ROTReceiver, RandomSender as ROTSender},
+    ot::{KosReceiver, KosSender},
     svole::{
         base_svole::{Receiver as VoleReceiver, Sender as VoleSender},
         copee::{Receiver as CpReceiver, Sender as CpSender},
-        CopeeReceiver,
-        CopeeSender,
         SVoleReceiver,
         SVoleSender,
     },
 };
 use rand::SeedableRng;
-//use rand_core::{CryptoRng, RngCore};
 use scuttlebutt::{
     field::{FiniteField as FF, Fp, Gf128},
     AesRng,
     Block,
     Channel,
-    Malicious,
 };
 use std::{
     io::{BufReader, BufWriter},
@@ -38,16 +34,12 @@ use std::{
 const T: usize = 1 << 10;
 
 fn bench_svole_<
-    ROTS: ROTSender + Malicious,
-    ROTR: ROTReceiver + Malicious,
-    FE: FF + Sync + Send,
-    CPS: CopeeSender<Msg = FE>,
-    CPR: CopeeReceiver<Msg = FE>,
-    BVSender: SVoleSender<Msg = FE> + Sync + Send,
+    FE: FF,
+    BVSender: SVoleSender<Msg = FE> + Sync + Send + 'static,
     BVReceiver: SVoleReceiver<Msg = FE> + Sync + Send,
 >(
-    sender_: Arc<Mutex<BVSender>>,
-    receiver_: Arc<Mutex<BVReceiver>>,
+    sender_: &mut Arc<Mutex<BVSender>>,
+    receiver_: &mut Arc<Mutex<BVReceiver>>,
     len: usize,
 ) {
     let (sender, receiver) = UnixStream::pair().unwrap();
@@ -70,60 +62,53 @@ fn bench_svole_<
     handle.join().unwrap();
 }
 
-type BVSender<FE: FF> = VoleSender<CpSender<KosSender, FE>, FE>;
-type BVReceiver<FE: FF> = VoleReceiver<CpReceiver<KosReceiver, FE>, FE>;
+type BVSender<FE> = VoleSender<CpSender<KosSender, FE>, FE>;
+type BVReceiver<FE> = VoleReceiver<CpReceiver<KosReceiver, FE>, FE>;
+
 fn bench_svole_fp(c: &mut Criterion) {
-    c.bench_function("svole::", move |bench| {
+    c.bench_function("svole::send::receive::Fp", move |bench| {
         let (sender, _receiver) = UnixStream::pair().unwrap();
         let seed = rand::random::<Block>();
         let mut rng = AesRng::from_seed(seed);
         let reader = BufReader::new(sender.try_clone().unwrap());
         let writer = BufWriter::new(sender);
         let mut channel = Channel::new(reader, writer);
-        let svole_sender = Arc::new(Mutex::new(
+        let mut svole_sender = Arc::new(Mutex::new(
             BVSender::<Fp>::init(&mut channel, &mut rng).unwrap(),
         ));
-        let svole_receiver = Arc::new(Mutex::new(
+        let mut svole_receiver = Arc::new(Mutex::new(
             BVReceiver::<Fp>::init(&mut channel, &mut rng).unwrap(),
         ));
         bench.iter(move || {
-            bench_svole_::<
-                KosSender,
-                KosReceiver,
-                Fp,
-                CpSender<KosSender, Fp>,
-                CpReceiver<KosReceiver, Fp>,
-                VoleSender<CpSender<KosSender, Fp>, Fp>,
-                VoleReceiver<CpReceiver<KosReceiver, Fp>, Fp>,
-            >(svole_sender, svole_receiver, T);
+            bench_svole_::<Fp, BVSender<Fp>, BVReceiver<Fp>>(
+                &mut svole_sender,
+                &mut svole_receiver,
+                T,
+            );
         })
     });
 }
 
 fn bench_svole_gf128(c: &mut Criterion) {
-    c.bench_function("svole::", move |bench| {
+    c.bench_function("svole::send::receive::Gf128", move |bench| {
         let (sender, _receiver) = UnixStream::pair().unwrap();
         let seed = rand::random::<Block>();
         let mut rng = AesRng::from_seed(seed);
         let reader = BufReader::new(sender.try_clone().unwrap());
         let writer = BufWriter::new(sender);
         let mut channel = Channel::new(reader, writer);
-        let svole_sender = Arc::new(Mutex::new(
+        let mut svole_sender = Arc::new(Mutex::new(
             BVSender::<Gf128>::init(&mut channel, &mut rng).unwrap(),
         ));
-        let svole_receiver = Arc::new(Mutex::new(
+        let mut svole_receiver = Arc::new(Mutex::new(
             BVReceiver::<Gf128>::init(&mut channel, &mut rng).unwrap(),
         ));
         bench.iter(move || {
-            bench_svole_::<
-                KosSender,
-                KosReceiver,
-                Gf128,
-                CpSender<KosSender, Gf128>,
-                CpReceiver<KosReceiver, Gf128>,
-                VoleSender<CpSender<KosSender, Gf128>, Gf128>,
-                VoleReceiver<CpReceiver<KosReceiver, Gf128>, Gf128>,
-            >(svole_sender, svole_receiver, T);
+            bench_svole_::<Gf128, BVSender<Gf128>, BVReceiver<Gf128>>(
+                &mut svole_sender,
+                &mut svole_receiver,
+                T,
+            );
         })
     });
 }
