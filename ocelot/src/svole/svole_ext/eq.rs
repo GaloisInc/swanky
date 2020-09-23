a380c917ee04c2c2f51977d982b278dc9d819573
Ajay Kumar Eeralla <aeeralla@galois.com>
2020-09-22 17:10:23 -0700

    clean the code

--- ocelot/src/svole/svole_ext/eq.rs
+++ ocelot/src/svole/svole_ext/eq.rs
@@ -42,21 +42,16 @@ impl<FE: FiniteField> EqSender for Sender<FE> {
         channel.read_bytes(&mut comm_vb)?;
         let mut seed = [0u8; 32];
         channel.read_bytes(&mut seed)?;
-        let vb_: Result<FE, _> = channel.read_fe();
-        match vb_ {
-            Ok(fe) => {
-                let mut commit = ShaCommitment::new(seed);
-                commit.input(&fe.to_bytes());
-                let res = commit.finish();
-                if res == comm_vb {
-                    Ok(va == fe)
-                } else {
-                    Err(Error::Other(
-                        "Failed Opening commitments in EQ protocol.".to_string(),
-                    ))
-                }
-            }
-            Err(e) => Err(Error::Other(e.to_string())),
+        let vb = channel.read_fe::<FE>()?;
+        let mut commit = ShaCommitment::new(seed);
+        commit.input(&vb.to_bytes());
+        let res = commit.finish();
+        if res == comm_vb {
+            Ok(va == vb)
+        } else {
+            Err(Error::Other(
+                "Failed Opening commitments in EQ protocol.".to_string(),
+            ))
         }
     }
 }
@@ -81,21 +76,16 @@ impl<FE: FiniteField> EqReceiver for Receiver<FE> {
         input: &FE,
     ) -> Result<bool, Error> {
         let vb = *input;
-        let va: Result<FE, _> = channel.read_fe();
+        let va = channel.read_fe::<FE>()?;
         let seed = rng.gen::<[u8; 32]>();
         let mut commit = ShaCommitment::new(seed);
         commit.input(&vb.to_bytes());
         let result = commit.finish();
         channel.write_bytes(&result)?;
-        match va {
-            Ok(fe) => {
-                channel.write_bytes(&seed)?;
-                channel.write_fe(vb)?;
-                channel.flush()?;
-                Ok(fe == vb)
-            }
-            Err(e) => Err(Error::Other(e.to_string())),
-        }
+        channel.write_bytes(&seed)?;
+        channel.write_fe(vb)?;
+        channel.flush()?;
+        Ok(va == vb)
     }
 }
 
