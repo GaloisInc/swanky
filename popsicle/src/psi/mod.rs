fd903d95d9b83d42e40e9c0d77633acd09054c9f
Ra1issa <ra1issa@bu.edu>
2020-10-12 16:31:23 -0400

    Integrate psty_payload into the library

--- popsicle/src/psi/mod.rs
+++ popsicle/src/psi/mod.rs
@@ -6,6 +6,8 @@
 
 pub mod kmprt;
 #[cfg(feature = "psty")]
+pub mod psty_payload;
+#[cfg(feature = "psty")]
 pub mod psty;
 pub mod psz;
 
@@ -20,6 +22,13 @@ pub type ExtendedSender = psty::Sender;
 #[cfg(feature = "psty")]
 /// Extended private set intersection receiver.
 pub type ExtendedReceiver = psty::Receiver;
+
+#[cfg(feature = "psty")]
+/// Extended private set intersection sender.
+pub type ExtendedSenderPayload = psty_payload::Sender;
+#[cfg(feature = "psty")]
+/// Extended private set intersection receiver.
+pub type ExtendedReceiverPayload = psty_payload::Receiver;
 
 /// Multi-party private set intersection sender.
 pub type MultiPartySender = kmprt::Sender;
