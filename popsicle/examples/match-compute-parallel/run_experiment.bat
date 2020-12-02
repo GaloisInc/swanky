b83083e209a9b44eacf0c2bcf72b6105ffc90996
Ra1issa <ra1issa@bu.edu>
2020-12-02 11:07:05 -0500

    Small fixed

--- popsicle/examples/match-compute-parallel/run_experiment.bat
+++ popsicle/examples/match-compute-parallel/run_experiment.bat
@@ -1,11 +1,6 @@
-echo on
 echo "Starting Program"
 
 cd %UserProfile%\My Documents\swanky-master\popsicle
 
 start cargo run --example run_server --features="psty_payload"
 start cargo run --example run_client --features="psty_payload"
-
-echo "Done with Program"
-
-pause
