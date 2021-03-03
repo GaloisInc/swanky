efceb254de2ea9fd42d21689365b8c89c5308d06
Ra1issa <ra1issa@bu.edu>
2021-03-03 11:53:35 -0500

    Adding executables

---
+++ target/release/examples/run_client.d
@@ -0,0 +1,10 @@
+/Users/rissa/Documents/swanky/target/release/examples/run_client: popsicle/examples/match-compute-parallel/client/run_client.rs popsicle/examples/match-compute-parallel/client/prepare_files_client.rs popsicle/examples/match-compute-parallel/client/client_thread.rs popsicle/examples/match-compute-parallel/client/join_aggregates_client.rs popsicle/examples/match-compute-parallel/client/parse_files.rs popsicle/examples/match-compute-parallel/client/test.rs
+
+/Users/rissa/Documents/swanky/target/release/examples/run_client.d: popsicle/examples/match-compute-parallel/client/run_client.rs popsicle/examples/match-compute-parallel/client/prepare_files_client.rs popsicle/examples/match-compute-parallel/client/client_thread.rs popsicle/examples/match-compute-parallel/client/join_aggregates_client.rs popsicle/examples/match-compute-parallel/client/parse_files.rs popsicle/examples/match-compute-parallel/client/test.rs
+
+popsicle/examples/match-compute-parallel/client/run_client.rs:
+popsicle/examples/match-compute-parallel/client/prepare_files_client.rs:
+popsicle/examples/match-compute-parallel/client/client_thread.rs:
+popsicle/examples/match-compute-parallel/client/join_aggregates_client.rs:
+popsicle/examples/match-compute-parallel/client/parse_files.rs:
+popsicle/examples/match-compute-parallel/client/test.rs:
---
+++ target/release/examples/run_server.d
@@ -0,0 +1,9 @@
+/Users/rissa/Documents/swanky/target/release/examples/run_server: popsicle/examples/match-compute-parallel/server/run_server.rs popsicle/examples/match-compute-parallel/server/prepare_files_server.rs popsicle/examples/match-compute-parallel/server/server_thread.rs popsicle/examples/match-compute-parallel/server/join_aggregates_server.rs popsicle/examples/match-compute-parallel/server/parse_files.rs
+
+/Users/rissa/Documents/swanky/target/release/examples/run_server.d: popsicle/examples/match-compute-parallel/server/run_server.rs popsicle/examples/match-compute-parallel/server/prepare_files_server.rs popsicle/examples/match-compute-parallel/server/server_thread.rs popsicle/examples/match-compute-parallel/server/join_aggregates_server.rs popsicle/examples/match-compute-parallel/server/parse_files.rs
+
+popsicle/examples/match-compute-parallel/server/run_server.rs:
+popsicle/examples/match-compute-parallel/server/prepare_files_server.rs:
+popsicle/examples/match-compute-parallel/server/server_thread.rs:
+popsicle/examples/match-compute-parallel/server/join_aggregates_server.rs:
+popsicle/examples/match-compute-parallel/server/parse_files.rs:
