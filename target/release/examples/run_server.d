e89816e61747cb61298e4e42721103950aaea972
Rawane Issa <rawaneissa@gmail.com>
2021-03-03 14:40:05 -0500

    Add windows released executables

---
+++ target/release/examples/run_server.d
@@ -0,0 +1,9 @@
+C:\Users\rawan\Documents\GitHub\swanky\target\release\examples\run_server.exe: popsicle\examples/match-compute-parallel/server/run_server.rs popsicle\examples/match-compute-parallel/server\prepare_files_server.rs popsicle\examples/match-compute-parallel/server\server_thread.rs popsicle\examples/match-compute-parallel/server\join_aggregates_server.rs popsicle\examples/match-compute-parallel/server\parse_files.rs
+
+C:\Users\rawan\Documents\GitHub\swanky\target\release\examples\run_server.d: popsicle\examples/match-compute-parallel/server/run_server.rs popsicle\examples/match-compute-parallel/server\prepare_files_server.rs popsicle\examples/match-compute-parallel/server\server_thread.rs popsicle\examples/match-compute-parallel/server\join_aggregates_server.rs popsicle\examples/match-compute-parallel/server\parse_files.rs
+
+popsicle\examples/match-compute-parallel/server/run_server.rs:
+popsicle\examples/match-compute-parallel/server\prepare_files_server.rs:
+popsicle\examples/match-compute-parallel/server\server_thread.rs:
+popsicle\examples/match-compute-parallel/server\join_aggregates_server.rs:
+popsicle\examples/match-compute-parallel/server\parse_files.rs:
