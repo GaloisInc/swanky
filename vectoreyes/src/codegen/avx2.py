492d14db4b248a8511e59acfef5b6431b652265e
Marc Rosen <marc@galois.com>
2021-03-22 21:49:06 -0400

    Add vectoreyes

---
+++ vectoreyes/src/codegen/avx2.py
@@ -0,0 +1,440 @@
+import dbm
+import json
+import lzma
+import subprocess
+import xml.etree.ElementTree as ET
+from collections import namedtuple
+from hashlib import sha256
+from pathlib import Path
+
+from cgtypes import *
+
+INTEL_INTRINSICS_XML_XZ = (
+    Path(__file__).resolve().parent / "intel-intrinsics-3.4.5.xml.xz"
+)
+with lzma.open(INTEL_INTRINSICS_XML_XZ) as compressed_xml:
+    INTEL_INTRINSICS_XML = {
+        x.get("name"): x for x in ET.parse(compressed_xml).getroot()
+    }
+
+# This is a compressed version of the October 2020 XML table from uops.info
+UOPS_INFO_XML_XZ = Path(__file__).resolve().parent / "uops.info-Oct2020.xml.xz"
+
+# NOTE: update version v every time the extraction algorithm changes to ensure that the cache never becomes stale.
+UOPS_INFO_DB = UOPS_INFO_XML_XZ.with_suffix(".v1.dbm")
+if not UOPS_INFO_DB.exists():
+    _Latency = namedtuple("_Latency", "value is_exact")
+    with dbm.open(str(UOPS_INFO_DB), "n") as db:
+        with lzma.open(str(UOPS_INFO_XML_XZ)) as compressed_xml:
+            UOPS_XML = ET.parse(compressed_xml)
+
+        def perf(arch):
+            measurement = arch.find("measurement")
+            if measurement is None:
+                return None
+            min_latency = _Latency(0x10000, True)
+            max_latency = _Latency(0, False)
+            for latency_elem in measurement.findall("latency"):
+                for k, v in latency_elem.attrib.items():
+                    if "cycles" not in k or "upper_bound" in k:
+                        continue
+                    l = _Latency(int(v), not latency_elem.get(f"{k}_is_upper_bound"))
+                    min_latency = min(l, min_latency)
+                    max_latency = max(l, max_latency)
+            return dict(
+                # TODO: is "unrolled" the right "default" to fall back to?
+                throughput=measurement.get("TP", measurement.get("TP_unrolled")),
+                min_latency=min_latency._asdict(),
+                max_latency=max_latency._asdict(),
+            )
+
+        for ext in UOPS_XML.getroot():
+            for insn in ext:
+                iform = insn.get("iform")
+                if not iform:
+                    continue
+                key = iform.encode("ascii")
+                if key in db:
+                    db[key] = b'{"has_duplicate": true}'
+                    continue
+                data = dict(
+                    url=insn.get("url"),
+                    ref_url=insn.get("url-ref"),
+                    string=insn.get("string"),
+                    summary=insn.get("summary"),
+                    perf={
+                        arch.get("name"): perf(arch)
+                        for arch in insn.findall("architecture")
+                        if arch.find("measurement")
+                    },
+                )
+                db[key] = json.dumps(data).encode("ascii")
+UOPS_INFO_DB = dbm.open(str(UOPS_INFO_DB))
+
+
+def uops_info(iform):
+    key = iform.encode("ascii")
+    if key not in UOPS_INFO_DB:
+        return None
+    return json.loads(UOPS_INFO_DB[key])
+
+
+class IntelInstruction(namedtuple("IntelInstruction", "xml uops_info")):
+    @property
+    def name(self):
+        return self.xml.get("name")
+
+    @property
+    def form(self):
+        return self.xml.get("form")
+
+    @property
+    def xed_form(self):
+        return self.xml.get("xed")
+
+    @property
+    def reference_url(self):
+        return "https://" + self.uops_info["ref_url"]
+
+    @property
+    def perf_url(self):
+        return "https://" + self.uops_info["url"]
+
+    @property
+    def summary(self):
+        return self.uops_info["summary"]
+
+    @property
+    def perf(self):
+        return self.uops_info["perf"]
+
+    def __str__(self):
+        if self.uops_info:
+            return self.uops_info["string"]
+        else:
+            return f"{self.name} {self.form}"
+
+
+class IntelIntrinsic(namedtuple("IntelIntrinsic", "name")):
+    @property
+    def xml(self):
+        return INTEL_INTRINSICS_XML[self.name]
+
+    def intel_reference_url(self):
+        return f"https://software.intel.com/sites/landingpage/IntrinsicsGuide/#text={self.name}"
+
+    def sequence(self):
+        return (self.xml.get("sequence") or "").lower() == "true"
+
+    def cpuid(self):
+        return [x.text for x in self.xml.findall("CPUID")]
+
+    def instructions(self):
+        return [
+            IntelInstruction(x, uops_info(x.get("xed")))
+            for x in self.xml.findall("instruction")
+        ]
+
+    def __str__(self):
+        return self.name
+
+
+# Due to limitations on const generics, we need to do manipulation on `const` generics
+# in the intrinsic wrapper, rather than at a higher-level.
+IntrinsicImmediateArgumentOverride = namedtuple(
+    "IntrinsicImmediateArgumentOverride", "const_args immediate_body"
+)
+
+
+def _make_immediate_argument_overrides():
+    shuffle = IntrinsicImmediateArgumentOverride(
+        const_args={f"I{i}": "usize" for i in reversed(range(4))},
+        immediate_body="((I3 << 6) | (I2 << 4) | (I1 << 2) | I0) as i32",
+    )
+    if_true = lambda flag, value: f"((!(({flag} as u64).wrapping_sub(1))) & {value})"
+    hi1 = if_true("HI1", "0xf0")
+    hi0 = if_true("HI0", "0x0f")
+    clmul = IntrinsicImmediateArgumentOverride(
+        const_args={"HI1": "bool", "HI0": "bool"},
+        immediate_body=f"({hi1} | {hi0}) as i32",
+    )
+    blend = lambda count: IntrinsicImmediateArgumentOverride(
+        const_args={f"B{i}": "bool" for i in reversed(range(count))},
+        immediate_body="("
+        + " | ".join(f"((B{i} as u8) << {i})" for i in range(count))
+        + ") as i32",
+    )
+    return {
+        "_mm256_shuffle_epi32": shuffle,
+        "_mm_shuffle_epi32": shuffle,
+        "_mm256_permute4x64_epi64": shuffle,
+        "_mm_clmulepi64_si128": clmul,
+        "_mm_blend_epi16": blend(8),
+        "_mm_blend_epi32": blend(4),
+        "_mm256_blend_epi32": blend(8),
+    }
+
+
+INTRINSIC_IMMEDIATE_ARGUMENT_OVERRIDES = _make_immediate_argument_overrides()
+
+
+class IntelIntrinsicBuilder:
+    # TODO: what microarchitectures do we care about?
+    # Use the naming convention from `rustc -C target-cpu=help`
+    TARGET_CPUS = [
+        "skylake",
+        "skylake-avx512",
+        "cascadelake",
+    ]
+    DISPLAY_PERF_NUMBERS_FOR = {
+        "Skylake": "SKL",
+        "Skylake-AVX512": "SKX",
+        "Cascade Lake": "CLX",
+    }
+    # None corresponds to the fallback value
+    # Otherwise, these numbers are the latncies of aes enc/dec instructions for their respsctive
+    # targets, since the throughputs for all these instructions are 1.
+    AES_BLOCK_COUNT_HINT = {
+        None: 8,
+        "skylake": 4,
+        "skylake-avx512": 4,
+        "cascadelake": 4,
+    }
+
+    def __init__(self, flags):
+        INHERENT_TO_x86_64 = {
+            "SSE2",
+        }
+        self.flags = set(flags) | INHERENT_TO_x86_64
+        self.needed_intrinsics = set()
+        self.AES_BLOCK_COUNT_HINT = IntelIntrinsicBuilder.AES_BLOCK_COUNT_HINT
+        self.DISPLAY_PERF_NUMBERS_FOR = IntelIntrinsicBuilder.DISPLAY_PERF_NUMBERS_FOR
+        self.TARGET_CPUS = IntelIntrinsicBuilder.TARGET_CPUS
+        for cpu in self.TARGET_CPUS:
+            assert cpu in self.AES_BLOCK_COUNT_HINT
+
+    def intrinsic_name(self, ty, op, ty2=None):
+        if ty == "raw":
+            return op
+        assert ty.bits in [256, 128]
+        suffixes = {8: "b", 16: "w", 32: "d", 64: "q"}
+        if ty.bits == 256:
+            prefix = "_mm256_"
+        elif ty.bits == 128:
+            prefix = "_mm_"
+        if ty.ty.signedness == Signedness.SIGNED:
+            iu = "i"
+        else:
+            iu = "u"
+        if op in [
+            "loadu",
+            "storeu",
+            "xor",
+            "and",
+            "or",
+            "testz",
+            "setzero",
+            "srli",
+            "slli",
+        ]:
+            return f"{prefix}{op}_si{ty.bits}"
+        elif op in [
+            "add_lanes",
+            "sub_lanes",
+            "extract",
+            "shuffle",
+            "cmpeq",
+            "cmpgt",
+            "movemask",
+            "blend",
+            "unpacklo",
+            "unpackhi",
+        ]:
+            if op == "cmpgt":
+                assert ty.ty.signedness == Signedness.SIGNED
+            core = op.replace("_lanes", "")
+            return f"{prefix}{core}_epi{ty.ty.bits}"
+        elif op == "broadcast_lo":
+            return f"{prefix}broadcast{suffixes[ty.ty.bits]}_epi{ty.ty.bits}"
+        elif op in ["broadcast", "set"]:
+            core = op
+            if core == "broadcast":
+                core = "set1"
+            out = f"{prefix}{core}_epi{ty.ty.bits}"
+            if ty.ty.bits == 64:
+                out += "x"
+            return out
+        elif op == "mul_lo_32":
+            return f"{prefix}mul_ep{iu}32"
+        elif op in ["max", "min"]:
+            return f"{prefix}{op}_ep{iu}{ty.ty.bits}"
+        elif op in ["shift_left_const", "shift_right_const"]:
+            if "right" in op:
+                if ty.ty.signedness == Signedness.SIGNED:
+                    return f"{prefix}srai_epi{ty.ty.bits}"
+                else:
+                    return f"{prefix}srli_epi{ty.ty.bits}"
+            else:
+                return f"{prefix}slli_epi{ty.ty.bits}"
+        elif op == "gather":
+            assert ty2 is not None
+            values = ty
+            indices = ty2
+            assert values.count == indices.count, repr((values, indices))
+            if indices.ty.bits == 32:
+                assert indices.ty.signedness == Signedness.SIGNED
+            if indices.bits == 256:
+                prefix = "_mm256_"
+            return f"{prefix}i{indices.ty.bits}gather_epi{values.ty.bits}"
+        elif op == "convert":
+            assert ty2 is not None
+            dst = ty
+            src = ty2
+            assert dst.ty.signedness == src.ty.signedness
+            assert dst.ty.bits > src.ty.bits
+            return f"{prefix}cvtep{iu}{src.ty.bits}_epi{dst.ty.bits}"
+        elif op == "permute":
+            assert ty.bits == 256
+            assert ty.ty.bits == 64
+            return "_mm256_permute4x64_epi64"
+        elif op in [
+            "shift_lo_left",
+            "shift_lo_right",
+            "shift_var_left",
+            "shift_var_right",
+        ]:
+            v = "v" if "var" in op else ""
+            if "left" in op:
+                return f"{prefix}sll{v}_epi{ty.ty.bits}"
+            else:
+                if ty.ty.signedness == Signedness.SIGNED:
+                    return f"{prefix}sra{v}_epi{ty.ty.bits}"
+                else:
+                    return f"{prefix}srl{v}_epi{ty.ty.bits}"
+        raise Exception(f"TODO: {repr(ty)}, {repr(op)}, {repr(ty2)}")
+
+    def __call__(self, ty, op, ty2=None):
+        intrinsic = self.intrinsic_name(ty, op, ty2)
+        self.needed_intrinsics.add(intrinsic)
+        return intrinsic
+
+    def convert_type(self, ty):
+        ty = ty.replace(" *", "*")
+        mapping = {
+            "__m128i": "::std::arch::x86_64::__m128i",
+            "__m256i": "::std::arch::x86_64::__m256i",
+            "__int32": "i32",
+            "unsigned int": "u32",
+            "int": "i32",
+            "char": "i8",
+            "short": "i16",
+            "const unsigned int": "u32",
+            "const int": "i32",
+            "__m64": "i64",
+            "long long": "i64",
+            "__m128i*": "*mut ::std::arch::x86_64::__m128i",
+            "__m256i*": "*mut ::std::arch::x86_64::__m256i",
+            "__m128i const*": "*const ::std::arch::x86_64::__m128i",
+            "__m256i const*": "*const ::std::arch::x86_64::__m256i",
+            "int const*": "*const i32",
+            "__int64 const*": "*const i64",
+            "__int64": "i64",
+        }
+        if ty in mapping:
+            return mapping[ty]
+        else:
+            raise Exception("TODO: handle type: " + repr(ty))
+
+    @property
+    def cfg_body(self):
+        # TODO: why can't we specify SSE4.1?
+        core = (
+            "all("
+            + ", ".join(
+                f"target_feature = {json.dumps(flag.lower())}"
+                for flag in sorted(list(self.flags))
+            )
+            + ")"
+        )
+        return f'all(target_arch="x86_64", {core})'
+
+    def define_intrinsics(self):
+        out = ""
+
+        def line(x):
+            nonlocal out
+            out += x
+            out += "\n"
+
+        line("#![allow(non_upper_case_globals, non_snake_case)]")
+        for name in sorted(list(self.needed_intrinsics)):
+            intrinsic = INTEL_INTRINSICS_XML[name]
+            assert intrinsic.tag == "intrinsic"
+            immediate_override = INTRINSIC_IMMEDIATE_ARGUMENT_OVERRIDES.get(name)
+            required_cpuid = set(x.text for x in intrinsic.findall("CPUID"))
+            missing_cpuid = required_cpuid - self.flags
+            if len(missing_cpuid) > 0:
+                raise Exception(
+                    f"Intrinsic {name} requires missing CPU flags {repr(missing_cpuid)}"
+                )
+            # TODO: this is the case for the intrinsics we care about. There are still
+            # some other unsafe intrinsics.
+            unsafe = any(
+                "*" in param.get("type") for param in intrinsic.findall("parameter")
+            )
+            unsafe = "unsafe " if unsafe else ""
+            immediates = {}
+            for param in intrinsic.findall("parameter"):
+                if param.get("etype") != "IMM":
+                    continue
+                # With current const generics support, we can't do `x as i32` if `x as i32`
+                # is destined for a const generic parameter. However, the immediate
+                # arguments to intrinsics are just any constant. So we can push the type
+                # coercion into the intrinsic's wrapper.
+                # TODO: do we always want usize for the immediates?
+                immediates[param.get("varname")] = "usize"
+            if immediate_override:
+                immediates = immediate_override.const_args
+            if len(immediates) == 0:
+                immediates = ""
+            else:
+                immediates = (
+                    "<"
+                    + ", ".join(f"const {k}: {v}" for k, v in immediates.items())
+                    + ">"
+                )
+            line("#[inline(always)]")
+            line(f"pub(super) {unsafe}fn {name}{immediates}(")
+            for param in intrinsic.findall("parameter"):
+                if param.get("etype") == "IMM":
+                    continue
+                if param.get("type") == "void":
+                    continue
+                line(f"{param.get('varname')}: {self.convert_type(param.get('type'))},")
+            rt = intrinsic.find("return").get("type")
+            if rt == "void":
+                line(") {")
+            else:
+                line(f") -> {self.convert_type(rt)} {{")
+            param_body = ", ".join(
+                immediate_override.immediate_body
+                if param.get("etype") == "IMM" and immediate_override
+                else param.get("varname")
+                + (
+                    f" as {self.convert_type(param.get('type'))}"
+                    if param.get("etype") == "IMM"
+                    else ""
+                )
+                for param in intrinsic.findall("parameter")
+                if param.get("type") != "void"
+            )
+            core = f"::std::arch::x86_64::{name}({param_body})"
+            if unsafe:
+                line(core)
+            else:
+                line(
+                    "// SAFETY: we've verified that the required CPU flags are available."
+                )
+                line(f"unsafe {{ {core} }}")
+            line("}")
+        return out
---
+++ vectoreyes/src/lib.rs
@@ -0,0 +1,333 @@
+//! VectorEyes is a (almost entirely) safe wrapper library around vectorized operations.
+//!
+//! # Backends
+//! VectorEyes chooses what backend to execute vector operations with at compile-time.
+//! ## AVX2
+//! CPUs that support the `AVX`, `AVX2`, `SSE4.1`, `AES`, `SSE4.2`, and `PCLMULQDQ` features will
+//! use the `AVX2` backend.
+//!
+//! In addition, we have embedded specific latency numbers for:
+//!
+//! * `skylake`
+//! * `skylake-avx512`
+//! * `cascadelake`
+//!
+//! As a result, `vectoreyes` will be more efficient on these platforms. You can add specific
+//! latency numbers for more targets in `avx2.py`.
+//! ## Scalar
+//! At the moment, this is the only alternative to the `AVX2` backend. It is not particularly
+//! optimized.
+//!
+//! # Cargo Configuration
+//! ## Native CPU Setup
+//! Compile on the machine that you'll be running your code on, and add the
+//! following to your `.cargo/config` file:
+//! ```toml
+//! [build]
+//! rustflags = ["-C", "target-cpu=native", "--cfg=vectoreyes-target-cpu-native"]
+//! rustdocflags = ["-C", "target-cpu=native", "--cfg=vectoreyes-target-cpu-native"]
+//! ```
+//! ## Specific CPU Selection
+//! If you want to compile for some specific CPU
+//! ```toml
+//! [build]
+//! rustflags = ["-C", "target-cpu=TARGET", "--cfg=vectoreyes-target-cpu=\"TARGET\""]
+//! rustdocflags = ["-C", "target-cpu=TARGET", "--cfg=vectoreyes-target-cpu=\"TARGET\""]
+//! ```
+//! ## Maximal Compatibility
+//! If you do not put any of the above in your `.cargo/config` file, vectoreyes will always use its
+//! `scalar` backend, which does not use vector instructions.
+//!
+//! **NOTE:** many functions are currently missing from this library. Please
+//! consult the Intel documentation to see if a non-implemented intrinsic would
+//! more directly accomplish your goal, and we can add it!
+
+// TODO: support more fine-grained cpu features?
+
+use std::ops::*;
+
+#[non_exhaustive]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum VectorBackend {
+    Scalar,
+    Avx2 { target_cpu: Option<&'static str> },
+}
+
+/// A scalar that can live in the lane of a vector.
+pub trait Scalar:
+    'static
+    + std::fmt::Debug
+    + num_traits::PrimInt
+    + num_traits::WrappingAdd
+    + num_traits::WrappingSub
+    + num_traits::WrappingMul
+{
+    /// A scalar of the same width as this scalar, but signed.
+    type Signed: Scalar;
+    /// A scalar of the same width as this scalar, but unsigned.
+    type Unsigned: Scalar;
+
+    /// A scalar of the same sign as this scalar, but with width 8.
+    type SameSign8: Scalar<Signed = i8, Unsigned = u8>;
+    /// A scalar of the same sign as this scalar, but with width 16.
+    type SameSign16: Scalar<Signed = i16, Unsigned = u16>;
+    /// A scalar of the same sign as this scalar, but with width 32.
+    type SameSign32: Scalar<Signed = i32, Unsigned = u32>;
+    /// A scalar of the same sign as this scalar, but with width 64.
+    type SameSign64: Scalar<Signed = i64, Unsigned = u64>;
+}
+macro_rules! scalar_impls {
+    ($(($s:ty, $u:ty)),*) => {$(
+        impl Scalar for $s {
+            type Signed = $s;
+            type Unsigned = $u;
+
+            type SameSign8 = i8;
+            type SameSign16 = i16;
+            type SameSign32 = i32;
+            type SameSign64 = i64;
+        }
+        impl Scalar for $u {
+            type Signed = $s;
+            type Unsigned = $u;
+
+            type SameSign8 = u8;
+            type SameSign16 = u16;
+            type SameSign32 = u32;
+            type SameSign64 = u64;
+        }
+    )*};
+}
+scalar_impls!((i64, u64), (i32, u32), (i16, u16), (i8, u8));
+/// A vector equivalent to `[T; Self::Lanes]`.
+///
+/// Note that each implemented method shows an equivalent scalar implementation.
+///
+/// # Effects of Signedness on shift operations
+/// When `T` is _signed_, this will shift in sign bits, as opposed to zeroes.
+pub trait SimdBase<T: Scalar>:
+    'static
+    + Sized
+    + Clone
+    + Copy
+    + Sync
+    + Send
+    + std::fmt::Debug
+    + PartialEq
+    + Eq
+    + Default
+    + bytemuck::Pod
+    + bytemuck::Zeroable
+    + BitXor
+    + BitXorAssign
+    + BitOr
+    + BitOrAssign
+    + BitAnd
+    + BitAndAssign
+    + AddAssign
+    + Add
+    + SubAssign
+    + Sub
+    + ShlAssign<u64>
+    + Shl<u64, Output = Self>
+    + ShrAssign<u64>
+    + Shr<u64, Output = Self>
+    + ShlAssign<Self>
+    + Shl<Self, Output = Self>
+    + ShrAssign<Self>
+    + Shr<Self, Output = Self>
+{
+    /// The number of elements of this vector.
+    ///
+    /// **Note:** this number is _not_ the number of 128-bit lanes in this vector.
+    const LANES: usize;
+
+    /// The equivalent array type of this vector.
+    type Array: 'static
+        + Sized
+        + Clone
+        + Copy
+        + Sync
+        + Send
+        + std::fmt::Debug
+        + PartialEq
+        + Eq
+        + Default
+        + std::hash::Hash
+        + AsRef<[T]>
+        + From<Self>
+        + Into<Self>;
+
+    /// The signed version of this vector.
+    type Signed: SimdBase<T::Signed> + From<Self> + Into<Self>;
+    /// The unsigned version of this vector.
+    type Unsigned: SimdBase<T::Unsigned> + From<Self> + Into<Self>;
+
+    const ZERO: Self;
+    fn is_zero(&self) -> bool;
+
+    /// Create a new vector by setting element 0 to `value`, and the rest of the elements to `0`.
+    fn set_lo(value: T) -> Self;
+
+    /// Create a new vector by setting every element to `value`.
+    fn broadcast(value: T) -> Self;
+
+    type BroadcastLoInput: SimdBase<T>;
+    /// Create a vector by setting every element to element 0 of `of`.
+    fn broadcast_lo(of: Self::BroadcastLoInput) -> Self;
+
+    /// Get the `I`-th element of this vector
+    fn extract<const I: usize>(&self) -> T;
+
+    /// Convert the vector to an array.
+    #[inline(always)]
+    fn as_array(&self) -> Self::Array {
+        (*self).into()
+    }
+
+    /// Shift each element left by `BITS`.
+    fn shift_left<const BITS: usize>(&self) -> Self;
+    /// Shift each element right by `BITS`.
+    /// # Effects of Signedness
+    /// When `T` is _signed_, this will shift in sign bits, as opposed to zeroes.
+    fn shift_right<const BITS: usize>(&self) -> Self;
+
+    /// Create a vector where each element is all 1's if the elements are equal, and all 0's otherwise.
+    fn cmp_eq(&self, other: Self) -> Self;
+    /// Create a vector where each element is all 1's if the element of `self` is greater than the
+    /// corresponding element of `other`, and all 0's otherwise.
+    fn cmp_gt(&self, other: Self) -> Self;
+
+    /// Interleave the elements of the low half of `self` and `other`
+    fn unpack_lo(&self, other: Self) -> Self;
+    /// Interleave the elements of the high half of `self` and `other`
+    fn unpack_hi(&self, other: Self) -> Self;
+
+    /// Make a vector consisting of the maximum elements of `self` and other.
+    fn max(&self, other: Self) -> Self;
+    /// Make a vector consisting of the minimum elements of `self` and other.
+    fn min(&self, other: Self) -> Self;
+}
+
+pub trait SimdBaseGatherable<T: Scalar, I: Scalar, IV: SimdBase<I>>: SimdBase<T> {
+    /// Construct a vector by accessing values at `base + indices[i]`
+    unsafe fn gather(base: *const T, indices: IV) -> Self;
+}
+
+// TODO: remove this?
+pub trait SimdBaseSigned<T: Scalar<Signed = T>>: SimdBase<T> {}
+
+/// A vector containing 4 lanes.
+pub trait SimdBase4x<T: Scalar>: SimdBase<T> {
+    /// If `Bi` is true, then that lane will be filled by `if_true`. Otherwise the lane
+    /// will be filled from `self`.
+    fn blend<const B3: bool, const B2: bool, const B1: bool, const B0: bool>(
+        &self,
+        if_true: Self,
+    ) -> Self;
+}
+
+/// A vector containing 8 lanes.
+pub trait SimdBase8x<T: Scalar>: SimdBase<T> {
+    /// If `Bi` is true, then that lane will be filled by `if_true`. Otherwise the lane
+    /// will be filled from `self`.
+    fn blend<
+        const B7: bool,
+        const B6: bool,
+        const B5: bool,
+        const B4: bool,
+        const B3: bool,
+        const B2: bool,
+        const B1: bool,
+        const B0: bool,
+    >(
+        &self,
+        if_true: Self,
+    ) -> Self;
+}
+
+/// A vector containing 8-bit values.
+pub trait SimdBase8<T: Scalar<Unsigned = u8, Signed = i8>>: SimdBase<T> {
+    /// Shift within 128-bit lanes.
+    fn shift_bytes_left<const AMOUNT: usize>(&self) -> Self;
+    /// Shift within 128-bit lanes.
+    fn shift_bytes_right<const AMOUNT: usize>(&self) -> Self;
+    /// Get the sign/most significant bits of the elements of the vector.
+    fn most_significant_bits(&self) -> u32;
+}
+
+/// A vector containing 32-bit values.
+pub trait SimdBase32<T: Scalar<Unsigned = u32, Signed = i32>>: SimdBase<T> {
+    /// Shuffle within 128-bit lanes.
+    fn shuffle<const I3: usize, const I2: usize, const I1: usize, const I0: usize>(&self) -> Self;
+}
+
+/// A vector containing 64-bit values.
+pub trait SimdBase64<T: Scalar<Unsigned = u64, Signed = i64>>: SimdBase<T> {
+    /// Zero out the upper-32 bits of each word, and then perform pairwise multiplication.
+    fn mul_lo(&self, other: Self) -> Self;
+}
+
+/// A vector containing 4 64-bit values.
+pub trait SimdBase4x64<T: Scalar<Unsigned = u64, Signed = i64>>:
+    SimdBase64<T> + SimdBase4x<T>
+{
+    /// Shuffle across 128-bit lanes.
+    fn shuffle<const I3: usize, const I2: usize, const I1: usize, const I0: usize>(&self) -> Self;
+}
+
+/// A utility trait you probably won't need to use. See [Simd].
+pub trait HasVector<const N: usize>: Scalar {
+    type Vector: SimdBase<Self>;
+}
+
+/// An alternative way of naming SIMD types.
+///
+/// # Example
+/// ```
+/// # use vectoreyes::*;
+/// type MyVector = Simd<u8, 16>; // The same as U8x16.
+/// ```
+pub type Simd<T, const N: usize> = <T as HasVector<N>>::Vector;
+
+pub trait AesBlockCipher: 'static + Clone + Sync + Send {
+    type Key: 'static + Clone + Sync + Send;
+
+    /// If you don't need to use Aes for decryption, it's faster to only perform key scheduling
+    /// for encryption than for both encryption and decryption.
+    type EncryptOnly: AesBlockCipher<Key = Self::Key> + From<Self>;
+
+    /// A pre-scheduled Aes block cipher with a compile-time constant key.
+    const FIXED_KEY: Self;
+
+    /// Running `encrypt_many` with this many blocks will result in the best performance.
+    ///
+    /// When using hardware AES instructions, if the AES encrypt instructions all have a
+    /// throughput of 1, then this constant will be equal to the instruction latency.
+    const BLOCK_COUNT_HINT: usize;
+
+    /// If you need to AES with a particular key, be careful about endianness issues.
+    fn new_with_key(key: Self::Key) -> Self;
+
+    #[inline(always)]
+    fn encrypt(&self, block: U8x16) -> U8x16 {
+        self.encrypt_many([block])[0]
+    }
+    fn encrypt_many<const N: usize>(&self, blocks: [U8x16; N]) -> [U8x16; N]
+    where
+        array_utils::ArrayUnrolledOps: array_utils::UnrollableArraySize<N>;
+}
+
+pub trait AesBlockCipherDecrypt: AesBlockCipher {
+    #[inline(always)]
+    fn decrypt(&self, block: U8x16) -> U8x16 {
+        self.decrypt_many([block])[0]
+    }
+    fn decrypt_many<const N: usize>(&self, blocks: [U8x16; N]) -> [U8x16; N]
+    where
+        array_utils::ArrayUnrolledOps: array_utils::UnrollableArraySize<N>;
+}
+
+pub mod array_utils;
+mod generated;
+pub use generated::implementation::*;
