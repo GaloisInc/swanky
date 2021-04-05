7d3e2b793858b5b7c80e1a0a8380c91c5b37ef3e
Marc Rosen <marc@galois.com>
2021-04-05 16:24:33 -0400

    Improve vectorized uniform integer generation

--- scuttlebutt/benches/rand_aes.rs
+++ scuttlebutt/benches/rand_aes.rs
@@ -7,9 +7,31 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use rand::distributions::{Distribution, Uniform};
 use rand_core::RngCore;
-use scuttlebutt::AesRng;
+use scuttlebutt::{AesRng, UniformIntegersUnderBound};
 
-fn bench_aes_rand(c: &mut Criterion) {
+#[cfg(not(target_os = "linux"))]
+mod measurement {
+    use criterion::measurement::WallTime;
+    pub(super) type Measurement = WallTime;
+
+    pub(super) fn new_measurement() -> Measurement {
+        WallTime
+    }
+}
+
+#[cfg(target_os = "linux")]
+mod measurement {
+    use criterion_linux_perf::{PerfMeasurement, PerfMode};
+    pub(super) type Measurement = PerfMeasurement;
+
+    pub(super) fn new_measurement() -> Measurement {
+        PerfMeasurement::new(PerfMode::Cycles)
+    }
+}
+
+use measurement::*;
+
+fn bench_aes_rand(c: &mut Criterion<Measurement>) {
     c.bench_function("AesRng::rand", |b| {
         let mut rng = AesRng::new();
         let mut x = (0..16 * 1024)
@@ -19,7 +41,7 @@ fn bench_aes_rand(c: &mut Criterion) {
     });
 }
 
-fn bench_aes_rand_int_108000(c: &mut Criterion) {
+fn bench_aes_rand_int_108000(c: &mut Criterion<Measurement>) {
     const BOUND: u32 = 108000;
     c.bench_function("AesRng::rand 32 integers under 108000", |b| {
         let mut rng = AesRng::new();
@@ -34,15 +56,16 @@ fn bench_aes_rand_int_108000(c: &mut Criterion) {
         "AesRng::uniform_integers_under_bound 32 integers under 108000",
         |b| {
             let mut rng = AesRng::new();
+            let dist = UniformIntegersUnderBound::new(BOUND);
             b.iter(|| {
-                black_box(rng.uniform_integers_under_bound::<BOUND>());
-                black_box(rng.uniform_integers_under_bound::<BOUND>());
+                black_box(dist.sample(&mut rng));
+                black_box(dist.sample(&mut rng));
             });
         },
     );
 }
 
-fn bench_aes_rand_int_126(c: &mut Criterion) {
+fn bench_aes_rand_int_126(c: &mut Criterion<Measurement>) {
     const BOUND: u32 = 126;
     c.bench_function("AesRng::rand 32 integers under 126", |b| {
         let mut rng = AesRng::new();
@@ -57,17 +80,21 @@ fn bench_aes_rand_int_126(c: &mut Criterion) {
         "AesRng::uniform_integers_under_bound 32 integers under 126",
         |b| {
             let mut rng = AesRng::new();
+            let dist = UniformIntegersUnderBound::new(BOUND);
             b.iter(|| {
-                black_box(rng.uniform_integers_under_bound::<BOUND>());
-                black_box(rng.uniform_integers_under_bound::<BOUND>());
+                black_box(dist.sample(&mut rng));
+                black_box(dist.sample(&mut rng));
             });
         },
     );
 }
 
+// NOTE: The CyclesPerByte is inaccurate, since it doesn't make any attempt to serialize execution.
+// It only calls rdstc. Rather than trying to implement the right thing, we'll just increase the
+// sample size and hope for the best.
 criterion_group! {
     name = aesrng;
-    config = Criterion::default();
+    config = Criterion::default().with_measurement(new_measurement()).sample_size(4096);
     targets = bench_aes_rand, bench_aes_rand_int_126, bench_aes_rand_int_108000
 }
 criterion_main!(aesrng);
--- scuttlebutt/src/rand_aes/vectorized.rs
+++ scuttlebutt/src/rand_aes/vectorized.rs
@@ -1,89 +1,127 @@
 use super::AesRng;
 use vectoreyes::{
-    array_utils::{ArrayAdjacentPairs, ArrayUnrolledExt},
+    array_utils::{ArrayAdjacentPairs, ArrayUnrolledExt, ArrayUnrolledOps, UnrollableArraySize},
     Aes128EncryptOnly, AesBlockCipher, SimdBase, SimdBase32, SimdBase64, U32x4, U32x8, U64x4,
-    U8x16,
 };
 
-// NOTE: these techniques can also be extended to random number generation with a non-constant
-// upper-bound. However, if the bound is not known in advance, you might need to make different
-// choices. This is important when, for example, speeding up a Fisher-Yates shuffle.
-// See https://www.pcg-random.org/posts/bounded-rands.html
-// See https://lemire.me/blog/2019/06/06/nearly-divisionless-random-integer-generation-on-various-systems/
-// See https://github.com/colmmacc/s2n/blob/7ad9240c8b9ade0cc3a403a732ba9f1289934abd/utils/s2n_random.c#L187
-// See https://twitter.com/colmmacc/status/1311092454909599744
-// See https://github.com/apple/swift/pull/25286
-// This is an implementation of "Debiased Integer Multiplication — Lemire's Method"
-// See https://www.pcg-random.org/posts/bounded-rands.html
-#[inline(always)]
-fn lemire_inner_loop(
-    rand_bits: [U8x16; Aes128EncryptOnly::BLOCK_COUNT_HINT],
-    t: U32x8,
-    range: U64x4,
-) -> Option<[U32x8; Aes128EncryptOnly::BLOCK_COUNT_HINT / 2]> {
-    let x = rand_bits.array_map(
-        #[inline(always)]
-        |x| U64x4::from(U32x4::from(x)),
-    );
-    let m = x.array_map(
-        #[inline(always)]
-        |x| U32x8::from(x.mul_lo(range)),
-    );
-    let m_only_lo = m.array_map(
-        #[inline(always)]
-        |m| m.shuffle::<2, 0, 2, 0>(),
-    );
-    let m_lo = m_only_lo.pair_adjacent().array_map(
-        #[inline(always)]
-        |(a, b)| a.unpack_lo(b),
-    );
-    let l = m_lo;
-    // We reject if l < t.
-    if !l
-        .array_fold(
-            U32x8::ZERO,
-            #[inline(always)]
-            |acu, l| acu | l.cmp_gt(t),
-        )
-        .is_zero()
-    {
-        let m_only_hi = m.array_map(
-            #[inline(always)]
-            |m| m.shuffle::<3, 1, 3, 1>(),
-        );
-        // It doesn't matter whether we use hi or lo here.
-        let m_hi = m_only_hi.pair_adjacent().array_map(
-            #[inline(always)]
-            |(a, b)| a.unpack_hi(b),
-        );
-        Some(m_hi)
-    } else {
-        None
-    }
-}
-#[inline(always)]
-fn lemire_params(bound: u32) -> (U32x8, U64x4) {
-    let t = 0_u32.wrapping_sub(bound) % bound;
-    let t = U32x8::broadcast(t);
-    let range = U64x4::broadcast(bound as u64);
-    (t, range)
+/// Sample `u32`s uniformly from `[0, bound)`.
+#[derive(Debug, Clone, Copy)]
+pub struct UniformIntegersUnderBound {
+    threshold: u32,
+    bound: u32,
 }
 
-#[inline(always)]
-pub fn uniform_integers_under_bound<const BOUND: u32>(
-    rng: &mut AesRng,
-) -> [U32x8; Aes128EncryptOnly::BLOCK_COUNT_HINT / 2] {
-    debug_assert_eq!(Aes128EncryptOnly::BLOCK_COUNT_HINT % 2, 0);
-    if BOUND == 0 {
-        panic!("The bound cannot be zero!")
-    } else if BOUND == 1 {
-        Default::default()
-    } else {
-        let (t, range) = lemire_params(BOUND);
+impl UniformIntegersUnderBound {
+    /// Create the distribution.
+    ///
+    /// # Performance
+    /// This function performs very well as long as the bound, $`b`$ is such that $`t(b)`$ is small,
+    /// where:
+    /// ```math
+    /// t(b) = (2^{32} - b + 1) \mod b
+    /// ```
+    ///
+    /// In particular, the probability that a random vector of $`N`$ elements is accepted is:
+    /// ```math
+    /// \left(1 - \frac{t(b)}{2^{32}}\right)^N
+    /// ```
+    /// Thus, if $`t(b)`$ is quite small, then it is efficient for us to reject $`N`$ elements at a
+    /// time, as opposed to rejecting individual elements.
+    /// # Repeated Invocations
+    /// It is inefficient to repeatedly call `new` with a fresh `bound`. However, an alternative
+    /// algorithm has not yet been implemented.
+    /// # Timing Side-Channel
+    /// `bound` should be a _public_ value, since it may be leaked in the timing of sampling.
+    /// # Panics
+    /// Panics if `bound` is `0`.
+    // TODO: implement Lemire's "Nearly Divisionless" algorithm, too.
+    #[inline]
+    pub fn new(bound: u32) -> Self {
+        assert_ne!(bound, 0);
+        let threshold = 0_u32.wrapping_sub(bound) % bound;
+        UniformIntegersUnderBound { threshold, bound }
+    }
+
+    /// Produce `Aes128EncryptOnly::BLOCK_COUNT_HINT * 4` uniformly distributed `u32`s (under the
+    /// given bound).
+    #[inline(always)]
+    pub fn sample(&self, rng: &mut AesRng) -> [U32x8; Aes128EncryptOnly::BLOCK_COUNT_HINT / 2] {
+        debug_assert_eq!(Aes128EncryptOnly::BLOCK_COUNT_HINT % 2, 0);
+        const N: usize = Aes128EncryptOnly::BLOCK_COUNT_HINT;
+        const HALF_N: usize = Aes128EncryptOnly::BLOCK_COUNT_HINT / 2;
+        self.lemire_body::<N, HALF_N>(rng)
+    }
+
+    /// Produce 20 uniformly distributed `u32`s under the given bound.
+    ///
+    /// Random numbers are returned in `out[0][..]`, `out[1][..]`, and the even-indexed entries of
+    /// `out[2]` (i.e. `out[2][0]`, `out[2][2]`, `out[2][4]`, `out[2][6]`).
+    ///
+    /// # Alternatives
+    /// Consider using [Self::sample] instead. It may be faster on some platforms.
+    #[inline(always)]
+    pub fn sample_20(&self, rng: &mut AesRng) -> [U32x8; 3] {
+        const N: usize = 5;
+        const HALF_N: usize = 3;
+        self.lemire_body::<N, HALF_N>(rng)
+    }
+
+    // NOTE: these techniques can also be extended to random number generation with a non-constant
+    // upper-bound. However, if the bound is not known in advance, you might need to make different
+    // choices. This is important when, for example, speeding up a Fisher-Yates shuffle.
+    // See https://www.pcg-random.org/posts/bounded-rands.html
+    // See https://lemire.me/blog/2019/06/06/nearly-divisionless-random-integer-generation-on-various-systems/
+    // See https://github.com/colmmacc/s2n/blob/7ad9240c8b9ade0cc3a403a732ba9f1289934abd/utils/s2n_random.c#L187
+    // See https://twitter.com/colmmacc/status/1311092454909599744
+    // See https://github.com/apple/swift/pull/25286
+    // This is an implementation of "Debiased Integer Multiplication — Lemire's Method"
+    // See https://www.pcg-random.org/posts/bounded-rands.html
+    #[inline(always)]
+    fn lemire_body<const N: usize, const HALF_N: usize>(&self, rng: &mut AesRng) -> [U32x8; HALF_N]
+    where
+        ArrayUnrolledOps: UnrollableArraySize<N> + UnrollableArraySize<HALF_N>,
+        [U32x8; N]: ArrayAdjacentPairs<T = U32x8, AdjacentPairs = [(U32x8, U32x8); HALF_N]>,
+    {
+        let range = U64x4::broadcast(self.bound as u64);
+        let t = U32x8::broadcast(self.threshold);
         loop {
-            if let Some(out) = lemire_inner_loop(rng.random_bits(), t, range) {
-                break out;
+            let rand_bits = rng.random_bits_custom_size::<N>();
+            let x = rand_bits.array_map(
+                #[inline(always)]
+                |x| U64x4::from(U32x4::from(x)),
+            );
+            let m = x.array_map(
+                #[inline(always)]
+                |x| U32x8::from(x.mul_lo(range)),
+            );
+            let m_interleaved = m.array_map(
+                #[inline(always)]
+                |m| m.shuffle::<3, 1, 2, 0>(),
+            );
+            let m_lo = m_interleaved
+                .pair_adjacent_maybe_odd(U32x8::broadcast(u32::MAX))
+                .array_map(
+                    #[inline(always)]
+                    |(a, b)| a.unpack_lo(b),
+                );
+            let l = m_lo;
+            // We reject and try again if any element in l is less than self.threshold.
+            // In other words, we reject if the min(every element in l, self.threshold) != threshold
+            if l.array_fold(
+                t,
+                #[inline(always)]
+                |a, b| a.min(b),
+            ) != t
+            {
+                continue;
             }
+            let m_hi = m_interleaved
+                .pair_adjacent_maybe_odd(U32x8::ZERO)
+                .array_map(
+                    #[inline(always)]
+                    |(a, b)| a.unpack_hi(b),
+                );
+            break m_hi;
         }
     }
 }
@@ -91,20 +129,44 @@ pub fn uniform_integers_under_bound<const BOUND: u32>(
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::Block;
     use proptest::prelude::*;
+    use rand_core::SeedableRng;
+
+    // The 500_000 bound is chosen somewhat arbitrarily, to make sure that the thresholds aren't
+    // too big.
 
     proptest! {
         #[test]
         fn lemire_within_bounds(
-            rand_bits in any::<[[u8; 16]; Aes128EncryptOnly::BLOCK_COUNT_HINT]>(),
-            bound in any::<u32>(),
+            seed in any::<[u8; 16]>(),
+            bound in 1..=500_000_u32,
         ) {
-            let (t, range) = lemire_params(bound);
-            if let Some(out) = lemire_inner_loop(bytemuck::cast(rand_bits), t, range) {
-                for o in out.iter() {
-                    for x in o.as_array().iter().copied() {
-                        prop_assert!(x < bound);
+            let mut rng = AesRng::from_seed(Block::from(seed));
+            let dist = UniformIntegersUnderBound::new(bound);
+            for x in dist.sample(&mut rng).iter().copied() {
+                for y in x.as_array().iter().copied() {
+                    prop_assert!(y < bound);
+                }
+            }
+        }
+    }
+
+    proptest! {
+        #[test]
+        fn lemire_20_within_bounds(
+            seed in any::<[u8; 16]>(),
+            bound in 1..=500_000_u32,
+        ) {
+            let mut rng = AesRng::from_seed(Block::from(seed));
+            let dist = UniformIntegersUnderBound::new(bound);
+            let out = dist.sample_20(&mut rng);
+            for (i,x) in out.iter().copied().enumerate() {
+                for (j,y) in x.as_array().iter().copied().enumerate() {
+                    if i == 2 && (j % 2) == 1 {
+                        prop_assert_eq!(y, 0);
                     }
+                    prop_assert!(y < bound);
                 }
             }
         }
