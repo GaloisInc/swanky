93e499a4566839456db80ab1f64e159678ab8aef
Benoit Razet <benoit.razet@galois.com>
2021-08-06 14:26:45 -0400

    Run in one docker-compose swoop

--- ocelot/test_edabits.sh
+++ ocelot/test_edabits.sh
@@ -5,11 +5,12 @@ set -e
 echo "PROVER: ${PROVER}"
 echo "VERIFIER: ${VERIFIER}"
 
-if [[ ! -z "${PROVER}" ]]; then
-    sleep 20
-    cargo run --release --example lan_edabits -- -n 1000000 -b 3 -m 8 --quicksilver --prover
+if [[ ! -z "${VERIFIER}" ]]; then
+    tc qdisc add dev eth0 root netem delay 200ms
+    cargo run --release --example lan_edabits -- -n 1000000 -b 3 -m 8 --quicksilver --addr verifier:5527
 fi
 
-if [[ ! -z "${VERIFIER}" ]]; then
-    cargo run --release --example lan_edabits -- -n 1000000 -b 3 -m 8 --quicksilver
+if [[ ! -z "${PROVER}" ]]; then
+    sleep 20
+    cargo run --release --example lan_edabits -- -n 1000000 -b 3 -m 8 --quicksilver --prover --addr verifier:5527
 fi
