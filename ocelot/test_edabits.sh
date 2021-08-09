5a1d64747146d8d6cad412005e18049076ce5003
Benoit Razet <benoit.razet@galois.com>
2021-08-08 21:14:28 -0400

    Tweak tc command

--- ocelot/test_edabits.sh
+++ ocelot/test_edabits.sh
@@ -6,11 +6,13 @@ echo "PROVER: ${PROVER}"
 echo "VERIFIER: ${VERIFIER}"
 
 if [[ ! -z "${VERIFIER}" ]]; then
-    tc qdisc add dev eth0 root netem delay 200ms
-    cargo run --release --example lan_edabits -- -n 1000000 -b 3 -m 8 --quicksilver --addr verifier:5527
+    #tc qdisc add dev eth0 root netem delay 200ms
+    tc qdisc add dev eth0 root tbf rate 10000kbit latency 1000ms burst 1540
+    cargo run --release --example lan_edabits -- -n 1000000 -b 5 -m 8 --quicksilver --addr verifier:5527
 fi
 
 if [[ ! -z "${PROVER}" ]]; then
     sleep 20
-    cargo run --release --example lan_edabits -- -n 1000000 -b 3 -m 8 --quicksilver --prover --addr verifier:5527
+    tc qdisc add dev eth0 root tbf rate 10000kbit latency 1000ms burst 1540
+    cargo run --release --example lan_edabits -- -n 1000000 -b 5 -m 8 --quicksilver --addr verifier:5527 --prover
 fi
