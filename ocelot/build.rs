bf9619c0e54a0696939db51f8ef5d1f67fe4bebc
Benoit Razet <benoit.razet@galois.com>
2022-05-03 06:51:35 -0400

    Specialized build.rs for wasm32

--- ocelot/build.rs
+++ ocelot/build.rs
@@ -1,7 +1,13 @@
 extern crate cc;
 
-#[cfg(not(target_arch = "wasm32"))]
 fn main() {
+    let cargo_target_arch = std::env::var_os("CARGO_CFG_TARGET_ARCH");
+    if let Some(target_arch) = cargo_target_arch {
+        if target_arch == "wasm32" {
+            return;
+        }
+    }
+
     cc::Build::new()
         .file("cbits/transpose.c")
         .flag("-maes")
