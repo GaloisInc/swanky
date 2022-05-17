74b10ffaab727b1f21dae048dae6c2978dae90ce
Marc Rosen <marc@galois.com>
2022-05-17 16:28:00 -0400

    Use niv for vectoreyes nix

--- vectoreyes/src/codegen/default.nix
+++ vectoreyes/src/codegen/default.nix
@@ -1,7 +1,4 @@
-with (import (builtins.fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/386234e2a61e1e8acf94dfa3a3d3ca19a6776efb.tar.gz";
-  sha256 = "1qhfham6vhy67xjdvsmhb3prvsg854wfw4l4avxnvclrcm3k2yg8";
-})) {};
+with (import (import nix/sources.nix).nixpkgs) { };
 mkShell {
   buildInputs = [
     rustfmt
