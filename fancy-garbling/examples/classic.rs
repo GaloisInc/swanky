e8a98231790b7c507e1765c5006744280223bd16
Nathan Prat <nathan.prat@gmail.com>
2022-10-28 17:01:22 +0200

    WIP classic
    Add a bunch of inputs to try
    FAIL: wrong eval outputs??

--- fancy-garbling/examples/classic.rs
+++ fancy-garbling/examples/classic.rs
@@ -10,7 +10,79 @@ fn main() {
 
     println!("Total: {} ms", start.elapsed().unwrap().as_millis());
 
-    // TODO(interstellar) ???
+    // TODO(interstellar) ??? maybe: write to file, read file, eval (ie simulate full chain like api+download by app+eval)
+    // see also: fancy-garbling/src/garble.rs
+
+    // TODO(interstellar) how to refactor into "1 party computation"; current code is splitted into "garbler inputs" and "evaluator input"
+
+    for (ev_inputs, expected_result) in [
+        (
+            // 0
+            vec![0; 32],
+            // +1 = 1, no reminder
+            vec![
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 1, 0,
+            ],
+        ),
+        (
+            // 1
+            vec![
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 1,
+            ],
+            // +1 = 2, no reminder
+            vec![
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 1, 0, 0,
+            ],
+        ),
+        (
+            // X
+            vec![
+                1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0,
+                1, 0, 1, 0,
+            ],
+            // +1 = X+1, no reminder
+            vec![
+                1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0,
+                1, 0, 1, 1, 0,
+            ],
+        ),
+        (
+            // int32 max
+            vec![1; 32],
+            // +1 = full 0s with a reminder!
+            vec![
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 1,
+            ],
+        ),
+    ] {
+        let evaluator_inputs = &encoder.encode_evaluator_inputs(&ev_inputs);
+
+        // TODO(interstellar) ???
+        let mut gb_inputs = vec![0u16; 32];
+        if let Some(last) = gb_inputs.last_mut() {
+            *last = 1;
+        }
+        let garbler_inputs = &encoder.encode_garbler_inputs(&gb_inputs);
+
+        let res = garbled
+            .eval(&circ, garbler_inputs, evaluator_inputs)
+            .unwrap();
+
+        println!("ev_inputs      : {:?} ms", ev_inputs);
+        println!("res            : {:?} ms", res);
+        println!("expected_result: {:?} ms", expected_result);
+        // TODO interstellar
+        // assert_eq!(res, expected_result);
+        if (res != expected_result) {
+            println!("FAIL!");
+        } else {
+            println!("OK!");
+        }
+    }
 
     // TODO(interstellar) check eval results; or maybe instead in fancy-garbling/examples/semihonest_2pc.rs ?
 }
