eab689b93b6a67ac1c32d29b687f8fb02dec5663
Nathan Prat <nathan.prat@gmail.com>
2022-10-31 17:01:38 +0100

    WIP add skeleton for .skcd parser

--- fancy-garbling/examples/classic.rs
+++ fancy-garbling/examples/classic.rs
@@ -1,5 +1,44 @@
-use fancy_garbling::{circuit::Circuit, classic::garble};
+use fancy_garbling::{circuit::Circuit, circuit::CircuitBuilder, classic::garble};
 use std::time::SystemTime;
+
+// TODO(interstellar) use prost-build? or should we keep using pre-generated?
+// https://github.com/neoeinstein/protoc-gen-prost/issues/26
+#[allow(clippy::derive_partial_eq_without_eq)]
+mod interstellarpbapicircuits {
+    // include_bytes!(concat!(env!("OUT_DIR")), "/interstellarpbapicircuits.rs");
+    // include_bytes!(concat!(env!("OUT_DIR"), "/interstellarpbapicircuits.rs"));
+    //
+    // prost-build FAIL in enclave/SGX
+    // include!(concat!(env!("OUT_DIR"), "/interstellarpbapicircuits.rs"));
+    include!("../deps/protos/generated/rust/interstellarpbapicircuits.rs");
+}
+
+/// Errors emitted by the circuit parser.
+#[derive(Debug)]
+pub enum CircuitParserError {
+    /// An I/O error occurred.
+    IoError(std::io::Error),
+    /// A regular expression parsing error occurred.
+    RegexError(regex::Error),
+    /// An error occurred parsing an integer.
+    ParseIntError,
+    /// An error occurred parsing a line.
+    ParseLineError(String),
+    /// An error occurred parsing a gate type.
+    ParseGateError(String),
+}
+
+/// Parse a Protobuf-serialized .skcd file
+/// It is doing what fancy-garbling/src/parser.rs is doing for a "Blif Fashion" txt file,
+/// but for a .skcd instead.
+/// SKCD is essentially the same format, but with the gates written in a different order:
+/// - in "Bilf Fashion": gates are written "gate0_input0 gate0_input1 gate0_output gate0_type" etc
+/// - in SKCD: "gate0_input0 gate1_input0 gate2_input0" etc
+fn parse_skcd(filename: &str) -> Result<Circuit, CircuitParserError> {
+    let circ_builder = CircuitBuilder::new();
+
+    Ok(circ_builder.finish())
+}
 
 fn main() {
     let circ = Circuit::parse("circuits/adder_32bit.txt").unwrap();
