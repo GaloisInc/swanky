7f81ed1d07ce17fd725dc5ddafb8b1452e9b5fa0
Alex J. Malozemoff <amaloz@galois.com>
2022-11-09 09:17:00 -0800

    cleanup

--- inferno/src/utils.rs
+++ inferno/src/utils.rs
@@ -1,6 +1,6 @@
+use generic_array::typenum::Unsigned;
 use scuttlebutt::field::FiniteField;
 use simple_arith_circuit::Circuit;
-use generic_array::typenum::Unsigned;
 
 /// Compute the number of compression rounds. If there are no multiplication gates, we'll have zero rounds.
 pub(crate) fn nrounds<F: FiniteField>(circuit: &Circuit<F>, compression_factor: usize) -> usize {
@@ -23,17 +23,17 @@ pub(crate) fn validate_parameters<F: FiniteField>(
     repetitions: usize,
 ) -> bool {
     // XXX: Currently we only use the parameters from Table 4 in the Limbo paper,
-    // which only provides parameters for `F_{2^{64}}`.
+    // which provides valid parameter settings for `F_{2^{64}}` (and larger fields).
     if F::NumberOfBitsInBitDecomposition::USIZE >= 64 {
         match (nparties, compression_factor, repetitions) {
-            (16, 8, 40) => true,
-            (16, 16, 38) => true,
-            (32, 8, 34) => true,
-            (32, 16, 32) => true,
-            (64, 8, 30) => true,
-            (64, 16, 28) => true,
-            (128, 8, 27) => true,
-            (128, 16, 25) => true,
+            (16, 8, 40)
+            | (16, 16, 38)
+            | (32, 8, 34)
+            | (32, 16, 32)
+            | (64, 8, 30)
+            | (64, 16, 28)
+            | (128, 8, 27)
+            | (128, 16, 25) => true,
             _ => false,
         }
     } else {
