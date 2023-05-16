be45bf4a86d6dad1b3c858d333e12d0b140c6153
Alex J. Malozemoff <amaloz@galois.com>
2023-05-16 11:15:44 -0700

    Add `#![clippy(allow::all)]` to generated files

--- vectoreyes/build.rs
+++ vectoreyes/build.rs
@@ -25,7 +25,7 @@ fn main() {
         let last_whitespace = native_line.rfind(' ').expect("there is some whitespace");
         // The last two characters are ")."
         let target_cpu = &native_line[last_whitespace + 1..native_line.len() - 2];
-        if lines.find(|line| line.trim() == target_cpu).is_none() {
+        if !lines.any(|line| line.trim() == target_cpu) {
             panic!("target_cpu {:?} doesn't seem to be valid", target_cpu);
         }
         println!("cargo:rustc-cfg=vectoreyes_target_cpu={:?}", target_cpu);
