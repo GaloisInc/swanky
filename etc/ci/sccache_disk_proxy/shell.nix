80bcec0fa2ddc729a0d20e0f196d8bc21b9b99b4
Marc Rosen <marc@galois.com>
2023-06-14 14:28:51 -0700

    Fix sccache in CI
    It looks like sccache --start-server has been failing in CI due to a
    timeout error. As a result, sccache has not been capturing the
    environment variables and configuration of the sccache server, resulting
    in sccache not using the shared build cache.
    
    As a fallback, this commit additionally sets these same environment
    variables at the CI level.

--- etc/ci/sccache_disk_proxy/shell.nix
+++ etc/ci/sccache_disk_proxy/shell.nix
@@ -8,12 +8,7 @@ let sccache_disk_proxy = buildGoModule {
 in mkShell {
   buildInputs = [
     (writeShellScriptBin "start_sccache" ''
-      export SCCACHE_IDLE_TIMEOUT=0
-      export SCCACHE_ENDPOINT="127.64.64.1:9000"
-      export SCCACHE_BUCKET=swanky-sccache
-      export SCCACHE_S3_USE_SSL=off
-      export AWS_ACCESS_KEY_ID=galois
-      export AWS_SECRET_ACCESS_KEY=galoissecret
+      source ${./env.sh}
       tmp=$(mktemp -d)
       mkfifo "$tmp/ready"
       function cleanup() {
