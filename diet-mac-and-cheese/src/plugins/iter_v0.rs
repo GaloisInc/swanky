26f19dd0e6d99eedf532c17b2122f7e716df0430
Chris Phifer <cphifer@galois.com>
2023-06-28 15:52:14 -0700

    iter_v0.rs: Implement unrolling of iteration into calls.
    This commit implements the details of unrolling the iteration into
    calls.
    
    The bulk of the work was computing the wire ranges to pass to the
    calls; since we're unrolling at definition time rather than call
    time, we must refer to inputs through the wire ID assignments
    given by the spec's description of functions. This functionality
    is provided as a helper function since the index math is a bit
    weird.
    
    There are a number of TODO items relating to the counter input used
    in map_enumerated. In particular, we currently assume that the
    counter input is a single wire always, though the spec allows for
    any type/count of wires. The iter authors should be asked for
    clarification about the intended use of the counter input; I would
    have expected it to be like the condition wire from the mux plugin,
    where only F2 allowed for multiple wires, and all other fields use
    a single wire. If a wire can carry all of the finite field's values,
    it does not make sense to me that for larger fields, there is any
    value to multi-wire counter inputs (as big-endian values comprised
    of these wires would necessarily exceed the maximum field value
    when multiple wires are needed to represent.) The spec calls for
    cycling back to 0, but this makes the multi-wire non-F2 case even
    more odd to include.
    
    This implementation does not yet have any tests - so it might be
    completely wrong :) Proceed with caution!

--- diet-mac-and-cheese/src/plugins/iter_v0.rs
+++ diet-mac-and-cheese/src/plugins/iter_v0.rs
@@ -1,7 +1,10 @@
 use eyre::Context;
 use mac_n_cheese_sieve_parser::PluginTypeArg;
 
-use crate::circuit_ir::{FunStore, FuncDecl, GatesBody, TypeId, TypeStore, WireCount};
+use crate::circuit_ir::{
+    first_unused_wire_id, FunStore, FuncDecl, GateM, GatesBody, TypeId, TypeStore, WireCount,
+    WireId, WireRange,
+};
 
 use super::Plugin;
 
@@ -18,6 +21,24 @@ impl Plugin for IterV0 {
         _type_store: &TypeStore,
         fun_store: &FunStore,
     ) -> eyre::Result<GatesBody> {
+        fn ranges_for_iteration(
+            which_iteration: u64,
+            mut starting_id: WireId,
+            iterated_counts: &[(TypeId, WireCount)],
+            f_counts: &[(TypeId, WireCount)],
+        ) -> Vec<WireRange> {
+            let mut res = Vec::with_capacity(f_counts.len());
+            for (&(_, wc), &(_, wc_f)) in iterated_counts.iter().zip(f_counts) {
+                let offset_start = starting_id + which_iteration * wc_f;
+
+                res.push((offset_start, offset_start + wc_f - 1));
+
+                starting_id += wc;
+            }
+
+            res
+        }
+
         let enumerated = match operation {
             "map" => false,
             "map_enumerated" => true,
@@ -131,7 +152,77 @@ impl Plugin for IterV0 {
             );
         }
 
-        let mut gates = Vec::with_capacity(iter_count as usize);
+        let mut gates = Vec::with_capacity(if enumerated {
+            2 * iter_count as usize
+        } else {
+            iter_count as usize
+        });
+
+        // Compute ID of the first input wire
+        let mut curr_input_wire = 0;
+        for (_, wc) in output_counts {
+            curr_input_wire += wc;
+        }
+
+        if enumerated {
+            let counter_type = f_input_counts[num_env as usize].0;
+            let mut counter_wire = first_unused_wire_id(output_counts, input_counts);
+
+            for i in 0..iter_count {
+                // TODO: This seems a little bit sus. Do we need to do something more clever?
+                gates.push(GateM::Constant(
+                    counter_type,
+                    counter_wire,
+                    Box::new(i.to_le_bytes().to_vec()),
+                ));
+
+                let outs = ranges_for_iteration(i, 0, output_counts, f_output_counts);
+
+                // Seed Vec of inputs with the closure environment inputs.
+                // After this loop, curr_input_wire will be the ID of the first
+                // 'normal' input wire.
+                let mut ins = vec![];
+                for &(_, wc) in &input_counts[..num_env as usize] {
+                    ins.push((curr_input_wire, curr_input_wire + wc - 1));
+                    curr_input_wire += wc;
+                }
+
+                // TODO: Assumes counter is on one wire, but the spec actually
+                // accepts any type/count for this input, where many wires are
+                // interpreted as a single big-endian number.
+                ins.push((counter_wire, counter_wire));
+
+                ins.append(&mut ranges_for_iteration(
+                    i,
+                    curr_input_wire,
+                    &input_counts[num_env as usize..],
+                    &f_input_counts[input_start..],
+                ));
+
+                gates.push(GateM::Call(Box::new((func_name.clone(), outs, ins))));
+
+                counter_wire += 1;
+            }
+        } else {
+            for i in 0..iter_count {
+                let outs = ranges_for_iteration(i, 0, output_counts, f_output_counts);
+
+                let mut ins = vec![];
+                for &(_, wc) in &input_counts[..num_env as usize] {
+                    ins.push((curr_input_wire, curr_input_wire + wc - 1));
+                    curr_input_wire += wc;
+                }
+                ins.append(&mut ranges_for_iteration(
+                    i,
+                    curr_input_wire,
+                    &input_counts[num_env as usize..],
+                    &f_input_counts[input_start..],
+                ));
+
+                gates.push(GateM::Call(Box::new((func_name.clone(), outs, ins))));
+            }
+        }
+
         Ok(GatesBody::new(gates))
     }
 }
