2529ff3b2087bb05fb72b6772ee201d811230a0e
Chris Phifer <cphifer@galois.com>
2023-09-05 16:36:53 -0700

    Add crate-level docs for party abstraction.

--- crates/party/src/lib.rs
+++ crates/party/src/lib.rs
@@ -1,3 +1,46 @@
+//! Support for types indexed by a _party_.
+//!
+//! The aim of this crate is code de-duplication in the context of multi-party
+//! protocols. In particular, it introduces the `Party` trait and `PartyEither`
+//! type, which may be used to distinguish data/code specific to the particular
+//! parties.
+//!
+//! NOTE: As of this writing, the crate is specifically intendeed for use in
+//! a zero-knowledge context. The two parties are named [`Prover`] and
+//! [`Verifier`], and the types exposed by the [`private`] module are named
+//! accordingly for the specific context in which the `Prover` party has
+//! access to privileged information. We are exploring generalizations of this
+//! that would allow, for example, use in contexts where there are > 2 parties
+//! (and more control over naming.)
+//!
+//! As a brief example of usage, consider the following types:
+//!
+//! ```
+//! struct ProverSpecificData;
+//! struct VerifierSpecificData;
+//! ```
+//!
+//! Using `Party` and `PartyEither`, we can then define a structure that
+//! contains all of the common data, _plus_ a field that is either a
+//! `ProverSpecificData` or `VerifierSpecificData` depending on context:
+//!
+//! ```
+//! struct Info<P: Party> {
+//!     // ... other fields common to both parties ...
+//!     party_specific_data: PartyEither<P, ProverSpecificData, VerifierSpecificData>,
+//! }
+//!
+//! We can make party-specific decisions at any time by inspecting `P::WHICH`:
+//!
+//! ```
+//! match P::WHICH {
+//!     WhichParty::Prover(e) => ...,
+//!     WhichParty::Verifier(e) => ...,
+//! }
+//! ```
+//!
+//! The `e` values are evidence used to maintain type-safety: Prover code that
+//! attempts to access verifier-only data (and vice-versa) will not compile.
 use std::fmt::Debug;
 use std::hash::Hash;
 use std::marker::PhantomData;
