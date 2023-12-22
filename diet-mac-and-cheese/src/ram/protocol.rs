de81f4c935bb72f0ec399b179a6bd35a56532100
Chris Phifer <cphifer@galois.com>
2023-12-22 15:57:45 -0600

    protocol.rs: Add constructor for DoraRam structure.
    This commit adds the necessary SVOLE type parameter to DoraRam,
    and provides a constructor method that creates a new structure
    instance from a DietMacAndCheese and MemorySpace.

--- diet-mac-and-cheese/src/ram/protocol.rs
+++ diet-mac-and-cheese/src/ram/protocol.rs
@@ -1,10 +1,12 @@
+use std::marker::PhantomData;
+
 use rustc_hash::FxHashMap;
 
 use scuttlebutt::AbstractChannel;
 use swanky_field::{FiniteField, IsSubFieldOf};
 use swanky_party::{private::ProverPrivate, Party};
 
-use crate::mac::Mac;
+use crate::{mac::Mac, svole_trait::SvoleT, DietMacAndCheese};
 
 use super::{tx::TxChannel, MemorySpace};
 
@@ -12,8 +14,9 @@ pub struct DoraRam<
     P: Party,
     V: IsSubFieldOf<F>,
     F: FiniteField,
-    C: AbstractChannel,
+    C: AbstractChannel + Clone,
     M: MemorySpace<V>,
+    SVOLE: SvoleT<P, V, F>,
 > where
     F::PrimeField: IsSubFieldOf<V>,
 {
@@ -22,4 +25,28 @@ pub struct DoraRam<
     memory: ProverPrivate<P, FxHashMap<Vec<V>, Vec<V>>>,
     rds: Vec<Vec<Mac<P, V, F>>>,
     wrs: Vec<Vec<Mac<P, V, F>>>,
+    _ph: PhantomData<SVOLE>,
+}
+
+impl<
+        P: Party,
+        V: IsSubFieldOf<F>,
+        F: FiniteField,
+        C: AbstractChannel + Clone,
+        M: MemorySpace<V>,
+        SVOLE: SvoleT<P, V, F>,
+    > DoraRam<P, V, F, C, M, SVOLE>
+where
+    F::PrimeField: IsSubFieldOf<V>,
+{
+    pub fn new(dmc: &mut DietMacAndCheese<P, V, F, C, SVOLE>, space: M) -> Self {
+        Self {
+            space,
+            ch: TxChannel::new(dmc.channel.clone(), Default::default()),
+            memory: Default::default(),
+            rds: vec![],
+            wrs: vec![],
+            _ph: PhantomData,
+        }
+    }
 }
