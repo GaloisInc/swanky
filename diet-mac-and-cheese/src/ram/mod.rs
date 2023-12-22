02929911cb0025be9eed8eddb8e9631e81fda687
Chris Phifer <cphifer@galois.com>
2023-12-22 15:37:27 -0600

    Add the MemorySpace trait (NOTE: May need to be modified for SIEVE RAM!)

--- diet-mac-and-cheese/src/ram/mod.rs
+++ diet-mac-and-cheese/src/ram/mod.rs
@@ -1,2 +1,11 @@
 mod perm;
 mod tx;
+
+pub trait MemorySpace<V> {
+    type Addr: AsRef<[V]>;
+    type Enum: Iterator<Item = Self::Addr>;
+
+    fn size(&self) -> usize;
+
+    fn enumerate(&self) -> Self::Enum;
+}
