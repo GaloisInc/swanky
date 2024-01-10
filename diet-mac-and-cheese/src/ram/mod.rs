b518208f4b3e547e3da83120d0e2d2ff2f6fe11f
Chris Phifer <cphifer@galois.com>
2024-01-10 14:52:58 -0800

    Add helper functions to collapse Vecs of wires (via affine combination).

--- diet-mac-and-cheese/src/ram/mod.rs
+++ diet-mac-and-cheese/src/ram/mod.rs
@@ -4,8 +4,35 @@ mod tx;
 
 use protocol::DoraRam;
 
+use crate::backend_trait::BackendT;
+
 const PRE_ALLOC_MEM: usize = 1 << 20;
 const PRE_ALLOC_STEPS: usize = (1 << 23) + PRE_ALLOC_MEM;
+
+fn combine<'a, B: BackendT>(
+    backend: &'a mut B,
+    mut elems: impl Iterator<Item = &'a B::Wire>,
+    x: B::FieldElement,
+) -> eyre::Result<B::Wire> {
+    let mut y = backend.copy(elems.next().unwrap())?;
+    for c in elems {
+        y = backend.mul_constant(&y, x)?;
+        y = backend.add(&y, c)?;
+    }
+    Ok(y)
+}
+
+pub(super) fn collapse_vec<B: BackendT>(
+    backend: &mut B,
+    elems: &[&[B:: Wire]],
+    x: B::FieldElement,
+) -> eyre::Result<Vec<B::Wire>> {
+    let mut out = Vec::with_capacity(elems.len());
+    for e in elems {
+        out.push(combine(backend, e.iter(), x)?);
+    }
+    Ok(out)
+}
 
 pub trait MemorySpace<V> {
     type Addr: AsRef<[V]>;
