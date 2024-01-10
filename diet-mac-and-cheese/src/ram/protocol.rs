44089c08670fdabeb39431e1df19d18d5473d214
Chris Phifer <cphifer@galois.com>
2024-01-10 14:00:27 -0800

    First attempt at translating Mathias' implementation of remove method.

--- diet-mac-and-cheese/src/ram/protocol.rs
+++ diet-mac-and-cheese/src/ram/protocol.rs
@@ -1,10 +1,13 @@
-use std::marker::PhantomData;
+use std::{iter, marker::PhantomData};
 
 use rustc_hash::FxHashMap;
 
 use scuttlebutt::AbstractChannel;
 use swanky_field::{FiniteField, IsSubFieldOf};
-use swanky_party::{private::ProverPrivate, Party};
+use swanky_party::{
+    private::{ProverPrivate, ProverPrivateCopy},
+    Party, WhichParty,
+};
 
 use crate::{mac::Mac, svole_trait::SvoleT, DietMacAndCheese};
 
@@ -54,5 +57,67 @@ where
             wrs: Vec::with_capacity(PRE_ALLOC_MEM + PRE_ALLOC_STEPS),
             _ph: PhantomData,
         }
+    }
+
+    pub fn remove(
+        &mut self,
+        dmc: &mut DietMacAndCheese<P, V, F, C, SVOLE>,
+        addr: &[Mac<P, V, F>],
+    ) -> eyre::Result<Vec<Mac<P, V, F>>> {
+        let mut flat: Vec<Mac<P, V, F>> = Vec::with_capacity(
+            self.space.addr_size() + self.space.value_size() + self.challenge_size,
+        );
+
+        match P::WHICH {
+            WhichParty::Prover(ev) => {
+                let val_addr: Vec<_> = addr.iter().map(|e| e.value().into_inner(ev)).collect();
+                let old = self
+                    .memory
+                    .as_mut()
+                    .into_inner(ev)
+                    .remove(&val_addr)
+                    .unwrap_or_else(|| {
+                        vec![V::default(); self.space.value_size() + self.challenge_size]
+                    });
+
+                for (i, elem) in iter::empty()
+                    .chain(addr.iter().copied())
+                    .chain(old.into_iter().map(|x| {
+                        let m = dmc
+                            .fcom
+                            .input1_prover(ev, &mut self.ch, &mut dmc.rng, x)
+                            .unwrap();
+                        Mac::new(ProverPrivateCopy::new(x), m)
+                    }))
+                    .enumerate()
+                {
+                    flat[i] = elem;
+                }
+            }
+            WhichParty::Verifier(ev) => {
+                for (i, elem) in iter::empty()
+                    .chain(
+                        dmc.fcom
+                            .input_verifier(
+                                ev,
+                                &mut self.ch,
+                                &mut dmc.rng,
+                                self.space.value_size() + self.challenge_size,
+                            )
+                            .unwrap(),
+                    )
+                    .enumerate()
+                {
+                    flat[i] = elem;
+                }
+            }
+        }
+
+        self.rds.push(flat.clone());
+        Ok(
+            flat[self.space.addr_size()..self.space.addr_size() + self.space.value_size()]
+                .try_into()
+                .unwrap(),
+        )
     }
 }
