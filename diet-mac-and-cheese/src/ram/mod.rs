757c64d8cd676eb95be882a12eee706994e6ecdc
Chris Phifer <cphifer@galois.com>
2024-01-10 12:57:55 -0800

    Add methods to MemorySpace for the address/value sizes.

--- diet-mac-and-cheese/src/ram/mod.rs
+++ diet-mac-and-cheese/src/ram/mod.rs
@@ -8,6 +8,10 @@ pub trait MemorySpace<V> {
     type Addr: AsRef<[V]>;
     type Enum: Iterator<Item = Self::Addr>;
 
+    fn addr_size(&self) -> usize;
+
+    fn value_size(&self) -> usize;
+
     fn size(&self) -> usize;
 
     fn enumerate(&self) -> Self::Enum;
