ceac2bf9e21df9ee71c76ff43c2fbd3d7e37f0a8
Marcella Hastings <marcella@galois.com>
2024-01-24 16:52:47 -0500

    schmivitz: add insecure VOLE decommitment methods
    Adds decommitment plus a useful decommitment type with accessor methods
    for verification

--- schmivitz/src/vole/insecure.rs
+++ schmivitz/src/vole/insecure.rs
@@ -4,7 +4,7 @@
 //! ⚠️ This should be removed once there is a secure version of VOLE!
 //!
 
-use std::iter::repeat_with;
+use std::iter::{repeat_with, zip};
 
 use crate::parameters::{REPETITION_PARAM, VOLE_SIZE_PARAM};
 use eyre::{bail, eyre, Result};
@@ -33,9 +33,6 @@ struct InsecureVole {
     /// [`VOLE_SIZE_PARAM`] and $`\tau`$ is the [`REPETITION_PARAM`].
     masks: Vec<[F8b; REPETITION_PARAM]>,
 }
-
-struct InsecureCommitments;
-
 impl RandomVole for InsecureVole {
     type Decommitment = InsecureCommitments;
 
@@ -124,6 +121,83 @@ impl RandomVole for InsecureVole {
     }
 
     fn decommit(self, _transcript: &mut merlin::Transcript) -> Self::Decommitment {
-        todo!()
+        // NB: in a real protocol, we would decommit based on a challenge pulled from the
+        // transcript. This is fully insecure, so there's no validation that the verifier
+        // could actually do.
+
+        // Compute uΔ^T (where Δ^T is the transpose of the verifier key)
+        let u_delta = self
+            .values
+            .iter()
+            .map(|ui| self.verifier_key.map(|delta| *ui * delta))
+            .collect::<Vec<_>>();
+
+        assert_eq!(u_delta.len(), self.extended_witness_length);
+
+        // Add V + uΔ^T
+        let verifier_commitments = zip(self.masks, u_delta)
+            .map(|(v, ud)| {
+                zip(v, ud)
+                    .map(|(v, ud)| v + ud)
+                    .collect::<Vec<_>>()
+                    .try_into()
+                    .unwrap()
+            })
+            .collect();
+
+        Self::Decommitment {
+            extended_witness_length: self.extended_witness_length,
+            verifier_key: self.verifier_key,
+            verifier_commitments,
+        }
+    }
+}
+
+#[allow(unused)]
+struct InsecureCommitments {
+    /// Number of VOLEs requested.
+    extended_witness_length: usize,
+
+    /// Verifier's chosen random key $`\bf \Delta`$.
+    verifier_key: [F8b; REPETITION_PARAM],
+
+    /// Commitments $`\bf Q`$ to the random values using the specified key and masks.
+    ///
+    /// Guarantee: This has length `extended_witness_length` $`+ r\tau `$, where $`r`$ is the
+    /// [`VOLE_SIZE_PARAM`] and $`\tau`$ is the [`REPETITION_PARAM`].
+    verifier_commitments: Vec<[F8b; REPETITION_PARAM]>,
+}
+
+#[allow(unused)]
+impl InsecureCommitments {
+    /// Get the length of the extended witness (e.g. the number of VOLEs requested).
+    pub(crate) fn extended_witness_length(&self) -> usize {
+        self.extended_witness_length
+    }
+
+    /// Get the lifted verifier key ($`\Delta`$ in the paper).
+    pub(crate) fn verifier_key(&self) -> F128b {
+        F8b::form_superfield(&self.verifier_key.into())
+    }
+
+    /// Get the VOLEs corresponding to the witness ($`\bf Q_{[1..\ell]}`$ in the paper).
+    ///
+    /// The output is guaranteed to be [`Self::extended_witness_length()`].
+    pub(crate) fn witness_voles(&self) -> &[[F8b; REPETITION_PARAM]] {
+        &self.verifier_commitments[0..self.extended_witness_length]
+    }
+
+    /// Get the lifted VOLEs corresponding to the mask for the aggregate commitment
+    /// ($`q_{\ell+1}, \dots, q_{\ell + r\tau}`$ in the paper).
+    pub(crate) fn mask_voles(&self) -> [F128b; REPETITION_PARAM * VOLE_SIZE_PARAM] {
+        // Lift the commitments -- we only want the last $`r\tau`$ of them, so we skip the first ones.
+        // This will panic if we constructed the type with the wrong length.
+        self.verifier_commitments
+            .iter()
+            .skip(self.extended_witness_length)
+            .map(|q| -> F128b { F8b::form_superfield(q.into()) })
+            .collect::<Vec<_>>()
+            .try_into()
+            .unwrap()
     }
 }
