27fb40f43402141c0a6d39802eb58972ae7b747d
Rawane Issa <rissa@galois.com>
2024-02-06 12:16:23 -0800

    fancy-examples/sum: Add fancy garbling for summation
    Summary: Create the fancy garbling equivalent of the in the clear summation.

--- fancy-garbling/examples/simple_sum.rs
+++ fancy-garbling/examples/simple_sum.rs
@@ -1,4 +1,26 @@
+//! An example that adds two secret numbers in a binary garbled circuit
+//! using fancy-garbling.
+use fancy_garbling::{
+    BinaryBundle, BinaryGadgets, Fancy, FancyArithmetic, FancyBinary, FancyReveal,
+};
 use std::env;
+
+/// The main fancy function which describes the garbled circuit for summation.
+fn fancy_sum<F>(
+    f: &mut F,
+    garbler_wires: BinaryBundle<F::Item>,
+    evaluator_wires: BinaryBundle<F::Item>,
+) -> Result<BinaryBundle<F::Item>, F::Error>
+where
+    F: FancyReveal + Fancy + BinaryGadgets + FancyBinary + FancyArithmetic,
+{
+    // The garbler and the evaluator's values are added together.
+    // For simplicity we assume that the addition will not result
+    // in a carry.
+    let sum = f.bin_addition_no_carry(&garbler_wires, &evaluator_wires)?;
+
+    Ok(sum)
+}
 
 fn sum_in_clear(gb_value: u128, ev_value: u128) -> u128 {
     gb_value + ev_value
