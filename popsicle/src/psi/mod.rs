379e11933041ffa6f0d3e61473b308f621cf0c21
Rawane Issa <rissa@galois.com>
2024-03-07 10:41:43 -0500

    popsicle: Remove the psty and psty_payload features
    These features are unecessary and are stoping popsicle from being consistently
    tested by the CI.

--- popsicle/src/psi/mod.rs
+++ popsicle/src/psi/mod.rs
@@ -1,8 +1,5 @@
 pub mod kmprt;
-
-#[cfg(feature = "psty")]
 pub mod psty;
-#[cfg(feature = "psty_payload")]
 pub mod psty_payload;
 pub mod psz;
 
@@ -11,17 +8,13 @@ pub type Sender = psz::Sender;
 /// Private set intersection receiver.
 pub type Receiver = psz::Receiver;
 
-#[cfg(feature = "psty")]
 /// Extended private psty intersection sender.
 pub type ExtendedSender = psty::Sender;
-#[cfg(feature = "psty")]
 /// Extended private set intersection receiver.
 pub type ExtendedReceiver = psty::Receiver;
 
-#[cfg(feature = "psty_payload")]
 /// Private set intersection with associated payloads sender.
 pub type SenderPayload = psty_payload::Sender;
-#[cfg(feature = "psty_payload")]
 /// Private set intersection with associated payloads receiver.
 pub type ReceiverPayload = psty_payload::Receiver;
 
