1cf671ba20c3dee4ab9f1467b6db65824e8183ed
Chris Phifer <cphifer@galois.com>
2024-03-20 13:47:59 -0700

    lib: Update crate-level documentation.

--- diet-mac-and-cheese/src/lib.rs
+++ diet-mac-and-cheese/src/lib.rs
@@ -1,19 +1,21 @@
 /*!
+A 'diet' implementation of the Mac'n'Cheese interactive ZKP protocol.
 
-`diet_mac_and_cheese` provides a diet/light implementation of the Mac'n'Cheese protocol.
+Diet Mac'n'Cheese (DMC) can be used a both an application and a library. See the
+`README` (and CLI help text) for information regarding the configuration and
+running of the prover and verifier applications.
 
-The library provides structures to operate a prover `DietMacAndCheeseProver` or a verifier `DietMacAndCheeseVerifier`
-with an interface to initialize and execute the protocol at a gate-by-gate level.
-Gates operates on input values that can be either public or private.
-When a gate takes on public values it produces a public value, and when a gate is provided private values
-then it produces a private value.
-Public and private values are ingested using the pair of functions `input_public()`/`input_private()`
-and they return public/private values whose type is exposed to to the user as `ValueProver` and `ValueVerifier`.
+At a high level, DMC provides the following:
 
-The `DietMacAndCheeseProver`/`DietMacAndCheeseVerfier` are almost identical at a high-level and differ
-solely on the `input_private()` function. Also the API satisfies the following invariant:
-if any function call returns an error then any subsequent gate function call
-will directly return an error.
+- In-memory representations of SIEVE IR (the circuit format) resources
+- A frontend to parse SIEVE IR from flatbuffers
+- A frontend to parse SIEVE IR from text
+- Party-generic and multithreaded implementations of sVOLE
+- A party-generic circuit evaluation interface
+
+There is an example of using DMC as a library in the crate's `examples`
+directory; this shows off everything but the SIEVE IR parsers. Additional detail
+can be found in the module documentation for each component.
 */
 mod backend_multifield;
 pub use backend_multifield::EvaluatorCirc;
