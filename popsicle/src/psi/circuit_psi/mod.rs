8fdd7e91463d86ee806dc88ca7fb0b88e4287892
Rawane Issa <rissa@galois.com>
2024-04-01 10:15:12 -0700

    popsicle/circuit_psi: Add a function that bundles input wires in CircuitInput
    This function allows us to reason about inputs not in terms of wires, but rather
    in terms of the elements that they represent.

--- popsicle/src/psi/circuit_psi/mod.rs
+++ popsicle/src/psi/circuit_psi/mod.rs
@@ -1,13 +1,20 @@
 //! Implementation of the Pinkas-Schneider-Tkachenko-Yanai "extended" private
 //! set intersection protocol (cf. <https://eprint.iacr.org/2019/241>).
-use crate::errors::Error;
-use fancy_garbling::BinaryBundle;
+use crate::{errors::Error, psi::circuit_psi::circuits::*};
+use fancy_garbling::{AllWire, BinaryBundle, Fancy, FancyBinary, FancyReveal};
 use rand::{CryptoRng, Rng, RngCore, SeedableRng};
 use scuttlebutt::{AbstractChannel, Block, Block512};
+use std::fmt::Debug;
 
 pub mod circuits;
 pub mod evaluator;
 pub mod garbler;
+
+// Byte representation of a set element
+pub const ELEMENT_SIZE: usize = 8;
+
+// Byte representation of a payload value
+pub const PAYLOAD_SIZE: usize = 8;
 
 /// Encoded Garbled Circuit PsiInputs
 pub struct CircuitInputs<F> {
@@ -21,6 +28,46 @@ pub struct CircuitInputs<F> {
     // when they share the same element with the sender
     // and otherwise receive a random mask
     pub(crate) masks: Option<Vec<F>>,
+}
+
+/// A function that takes a `CircuitInputs`` (created by a BasePsi) and groups the wires of
+/// its different parts into `BinaryBundle` for ease of use in a fancy garbled circuit.
+///
+/// For instance, `sender_set_elements`'s wires are grouped according to the set element size.
+/// This function allows us to reason about circuit inputs not in terms of individual wires, but
+/// rather in terms of the values that they represent.
+fn bundle_inputs<F, E>(
+    f: &mut F,
+    circuit_inputs: &CircuitInputs<F::Item>,
+) -> Result<
+    (
+        Vec<BinaryBundle<<F as Fancy>::Item>>, // bits that parties are intersecting on
+        Option<Vec<BinaryBundle<<F as Fancy>::Item>>>,
+        Option<Vec<BinaryBundle<<F as Fancy>::Item>>>,
+    ),
+    Error,
+>
+where
+    F: FancyBinary + FancyReveal + Fancy<Item = AllWire, Error = E>,
+    E: Debug,
+    Error: From<E>,
+{
+    let set = wires_to_bundle::<F>(&circuit_inputs.sender_set_elements, ELEMENT_SIZE * 8);
+
+    let mut sender_payloads = None;
+    let mut receiver_payloads = None;
+    if let Some(p) = &circuit_inputs.sender_payloads_masked.as_ref() {
+        sender_payloads = Some(fancy_unmask(
+            f,
+            &wires_to_bundle::<F>(p, PAYLOAD_SIZE * 8),
+            &wires_to_bundle::<F>(&circuit_inputs.masks.as_ref().unwrap(), PAYLOAD_SIZE * 8),
+        )?);
+        receiver_payloads = Some(wires_to_bundle::<F>(
+            circuit_inputs.receiver_payloads.as_ref().unwrap(),
+            PAYLOAD_SIZE * 8,
+        ));
+    }
+    Ok((set, sender_payloads, receiver_payloads))
 }
 
 /// A trait which describes the parties participating in the circuit
