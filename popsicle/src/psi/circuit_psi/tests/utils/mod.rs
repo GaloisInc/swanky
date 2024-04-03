8d9616a5c12c671c4133056d9c250d1b3e6ab950
Rawane Issa <rissa@galois.com>
2024-04-03 15:06:26 -0700

    popsicle/circuit_psi: Create proptest strategy for generating inputs
    This will be useful for almost all circuit psi tests. We also have strategies for payloads
    as u128 and Block512 because the later helps us avoid manually turning payloads as Vec<u128>
    into Vec<Block512> that circuit psi expects. We this strategy when we generally
    just want to check that the protocol has suceeded with no errors and do not need
    to read or operate on the payloads. We use the `arbitrary_payloads_u128` for the later case.

--- popsicle/src/psi/circuit_psi/tests/utils/mod.rs
+++ popsicle/src/psi/circuit_psi/tests/utils/mod.rs
@@ -10,12 +10,51 @@ use std::{
 };
 
 #[cfg(test)]
+use proptest::{collection, strategy::Strategy};
+
+#[cfg(test)]
 /// Turns a Unixstream into a scuttlebutt channel
 pub fn setup(stream: UnixStream) -> Channel<BufReader<UnixStream>, BufWriter<UnixStream>> {
     let reader = BufReader::new(stream.try_clone().unwrap());
     let writer = BufWriter::new(stream);
     let channel = Channel::new(reader, writer);
     channel
+}
+
+#[cfg(test)]
+/// Create a proptest strategy that creates sets of elements represented as vectors of bytes.
+/// Reminder: sets have unique elements, we ensure this by generating hashsets using proptests
+/// and turning them into vecs.
+pub fn arbitrary_unique_sets(size: usize, max_value: u128) -> impl Strategy<Value = Vec<Vec<u8>>> {
+    collection::hash_set(0..max_value, size).prop_map(|v| {
+        v.into_iter()
+            .map(|value| value.to_le_bytes().to_vec())
+            .collect()
+    })
+}
+
+#[cfg(test)]
+/// Create a proptest strategy that creates payloads represented as u128.
+pub fn arbitrary_payloads_u128(size: usize, max_value: u128) -> impl Strategy<Value = Vec<u128>> {
+    collection::vec(0..max_value, size)
+}
+
+#[cfg(test)]
+/// Create a proptest strategy that creates payloads represented as Block512.
+pub fn arbitrary_payloads_block125(
+    size: usize,
+    max_value: u128,
+) -> impl Strategy<Value = Vec<Block512>> {
+    collection::vec(0..max_value, size).prop_map(|v| {
+        v.into_iter()
+            .map(|value| {
+                let value_bytes = value.to_le_bytes();
+                let mut value_block512 = [0u8; 64];
+                value_block512[0..16].clone_from_slice(&value_bytes[..16]);
+                Block512::from(value_block512)
+            })
+            .collect()
+    })
 }
 
 #[cfg(test)]
