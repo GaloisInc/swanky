9c397a4320605365c9c42cd0e688f33070d82479
Rawane Issa <rissa@galois.com>
2024-04-03 15:01:48 -0700

    popsicle/circuit_psi: Move some of psty's testing utility functions to new mod

--- popsicle/src/psi/circuit_psi/tests/utils/mod.rs
+++ popsicle/src/psi/circuit_psi/tests/utils/mod.rs
@@ -1,7 +1,7 @@
 //! Various utility functionalities for tests
 
 #[cfg(test)]
-use scuttlebutt::Channel;
+use scuttlebutt::{Block, Block512, Channel};
 
 #[cfg(test)]
 use std::{
@@ -16,4 +16,31 @@ pub fn setup(stream: UnixStream) -> Channel<BufReader<UnixStream>, BufWriter<Uni
     let writer = BufWriter::new(stream);
     let channel = Channel::new(reader, writer);
     channel
+}
+
+#[cfg(test)]
+/// Create a vector of Block512, from a vector of u64s
+pub fn int_vec_block512(values: Vec<u128>, size: usize) -> Vec<Block512> {
+    values
+        .into_iter()
+        .map(|item| {
+            let value_bytes = item.to_le_bytes();
+            let mut res_block = [0_u8; 64];
+            res_block[0..size].clone_from_slice(&value_bytes[..size]);
+            Block512::from(res_block)
+        })
+        .collect()
+}
+
+#[cfg(test)]
+/// Create a vector of Block512, from a vector of u64s
+pub fn u8_vec_block(values: &[Vec<u8>], size: usize) -> Vec<Block> {
+    values
+        .into_iter()
+        .map(|item| {
+            let mut res_block = [0_u8; 16];
+            res_block[0..size].clone_from_slice(&item[..size]);
+            Block::from(res_block)
+        })
+        .collect()
 }
