2950baaf23fa57546459b19b257bcea3019fdf34
Rawane Issa <rissa@galois.com>
2024-04-03 17:19:02 -0700

    popsicle/circuit_psi: Create test that checks if the opprf on the sender side preserved the original set
    popsicle/circuit_psi: Create test that checks if the opprf on the receiver side preserved the original set
    
    popsicle/circuit_psi: Add helper function that checks that output of the opprf preserves the sets
    
    This function tests the output against the original set by only relying on standard rust functions.

--- popsicle/src/psi/circuit_psi/tests/test_opprf.rs
+++ popsicle/src/psi/circuit_psi/tests/test_opprf.rs
@@ -12,7 +12,7 @@ mod tests {
 
     use proptest::prelude::*;
     use scuttlebutt::{AesRng, Block512};
-    use std::os::unix::net::UnixStream;
+    use std::{collections::HashSet, os::unix::net::UnixStream};
     const SET_SIZE: usize = 1 << 8;
 
     const ELEMENT_MAX: u128 = u64::MAX as u128;
@@ -52,6 +52,24 @@ mod tests {
             (sender, receiver, result_opprf_sender, result_opprf_receiver)
         })
     }
+    // Check that the opprf preserves the original set by intersecting the party's opprf outputs
+    // with the original set. The idea is that if the intersection cardinality in both cases is
+    // equal to the original set cardinality, then the hash outputs includes that set
+    fn psty_check_opprf_set(
+        sender: OpprfSender,
+        receiver: OpprfReceiver,
+        set: &[Vec<u8>],
+    ) -> (usize, usize) {
+        let set_hash: HashSet<Block512> = HashSet::from_iter(u8_vec_block512(set, ELEMENT_SIZE));
+        let sender_table: HashSet<Block512> =
+            HashSet::from_iter(sender.state.unwrap().opprf_set_out);
+        let receiver_table: HashSet<Block512> =
+            HashSet::from_iter(receiver.state.unwrap().opprf_set_out.unwrap());
+
+        let intersection_size_sx = set_hash.intersection(&sender_table).count();
+        let intersection_size_rx = set_hash.intersection(&receiver_table).count();
+        (intersection_size_sx, intersection_size_rx)
+    }
     proptest! {
         #[test]
         fn test_psty_opprf_sender_succeeded(
@@ -62,7 +80,7 @@ mod tests {
         ){
 
             let (_, _, result_opprf_sender, _) = psty_up_to_opprf(&set, &payloads, seed_sx, seed_rx);
-            assert!(
+            prop_assert!(
                 !result_opprf_sender.is_err(),
                 "PSTY OPPRF failed on the sender side"
             );
@@ -76,9 +94,43 @@ mod tests {
         ){
 
             let (_, _, _, result_opprf_receiver) = psty_up_to_opprf(&set, &payloads, seed_sx, seed_rx);
-            assert!(
+            prop_assert!(
                 !result_opprf_receiver.is_err(),
                 "PSTY OPPRF failed on the receiver side"
+            );
+        }
+        #[test]
+        fn test_psty_opprf_sender_set_preserved(
+            seed_sx in any::<u64>(),
+            seed_rx in any::<u64>(),
+            set in arbitrary_unique_sets(SET_SIZE, ELEMENT_MAX),
+            payloads in arbitrary_payloads_block125(SET_SIZE, PAYLOAD_MAX)
+        ){
+
+            let (sender, receiver, _, _) = psty_up_to_opprf(&set, &payloads, seed_sx, seed_rx);
+            let (intersection_size_sx , _) = psty_check_opprf_set(sender, receiver, &set);
+
+            assert!(
+            intersection_size_sx
+                    == SET_SIZE,
+                "PSTY OpprfSender did not preserve the original set, the intersection of the tables is {} and should be {}", intersection_size_sx, SET_SIZE
+            );
+        }
+                #[test]
+        fn test_psty_opprf_receiver_set_preserved(
+            seed_sx in any::<u64>(),
+            seed_rx in any::<u64>(),
+            set in arbitrary_unique_sets(SET_SIZE, ELEMENT_MAX),
+            payloads in arbitrary_payloads_block125(SET_SIZE, PAYLOAD_MAX)
+        ){
+
+            let (sender, receiver, _, _) = psty_up_to_opprf(&set, &payloads, seed_sx, seed_rx);
+            let (_ , intersection_size_rx) = psty_check_opprf_set(sender, receiver, &set);
+
+            assert!(
+            intersection_size_rx
+                    == SET_SIZE,
+                "PSTY OpprfReceiver did not preserve the original set, the intersection of the tables is {} and should be {}", intersection_size_rx, SET_SIZE
             );
         }
     }
