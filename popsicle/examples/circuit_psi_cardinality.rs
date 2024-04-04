2ed25088023d9c76f0cc9f14d55aa6c14646e8c1
Rawane Issa <rissa@galois.com>
2024-04-04 11:11:28 -0700

    popsicle/circuit_psi: Add examples for circuit_psi_no_payloads

--- popsicle/examples/circuit_psi_cardinality.rs
+++ popsicle/examples/circuit_psi_cardinality.rs
@@ -11,42 +11,33 @@ use popsicle::{
 
 use fancy_garbling::{AllWire, BinaryBundle, BinaryGadgets, Fancy, FancyBinary, FancyReveal};
 use rand::Rng;
-use scuttlebutt::{AesRng, Block, Block512};
+use scuttlebutt::{AesRng, Block};
 use std::{fmt::Debug, os::unix::net::UnixStream, thread};
 const SET_SIZE: usize = 1 << 8;
 
-pub fn fancy_payload_sum<F, E>() -> impl FnMut(
+pub fn fancy_cardinality<F, E>() -> impl FnMut(
     &mut F,
     &[<F as Fancy>::Item],
     &[BinaryBundle<<F as Fancy>::Item>],
-    Option<Vec<BinaryBundle<<F as Fancy>::Item>>>,
-    Option<Vec<BinaryBundle<<F as Fancy>::Item>>>,
 ) -> Result<BinaryBundle<<F as Fancy>::Item>, Error>
 where
     F: FancyBinary + FancyReveal + Fancy<Item = AllWire, Error = E>,
     E: Debug,
     Error: From<E>,
 {
-    |f, intersect_bitvec, _, payload_a, payload_b| {
-        let mut acc = f.bin_constant_bundle(0, PAYLOAD_SIZE * 8)?;
-        let zero = f.bin_constant_bundle(0, PAYLOAD_SIZE * 8)?;
-
-        for (i, bit) in intersect_bitvec.iter().enumerate() {
-            let mux_a = f.bin_multiplex(bit, &zero, &payload_a.as_ref().unwrap()[i])?;
-            let mux_b = f.bin_multiplex(bit, &zero, &payload_b.as_ref().unwrap()[i])?;
-            let mul = f.bin_addition_no_carry(&mux_a, &mux_b)?;
-            acc = f.bin_addition_no_carry(&acc, &mul)?;
+    |f, intersect_bitvec, _| {
+        let mut acc = f.bin_constant_bundle(0, ELEMENT_SIZE * 8)?;
+        let one = f.bin_constant_bundle(1, ELEMENT_SIZE * 8)?;
+        let zero = f.bin_constant_bundle(0, ELEMENT_SIZE * 8)?;
+        for bit in intersect_bitvec {
+            let mux = f.bin_multiplex(bit, &zero, &one)?;
+            acc = f.bin_addition_no_carry(&acc, &mux)?;
         }
         Ok(acc)
     }
 }
 
-pub fn psty_payload_sum(
-    set_a: &[Vec<u8>],
-    set_b: &[Vec<u8>],
-    payload_a: &[Block512],
-    payload_b: &[Block512],
-) -> u128 {
+pub fn psty_cardinality(set_a: &[Vec<u8>], set_b: &[Vec<u8>]) -> u128 {
     let (sender, receiver) = UnixStream::pair().unwrap();
     thread::scope(|s| {
         let _ = s.spawn(|| {
@@ -55,10 +46,9 @@ pub fn psty_payload_sum(
             let mut gb = PsiGarbler::new(&mut channel, rng.gen::<Block>()).unwrap();
 
             let res = gb
-                .circuit_psi_psty::<OpprfSender, _, _>(
+                .circuit_psi_psty_no_payloads::<OpprfSender, _, _>(
                     set_a,
-                    Some(payload_a),
-                    &mut fancy_payload_sum::<Gb, _>(),
+                    &mut fancy_cardinality::<Gb, _>(),
                 )
                 .unwrap();
             gb.gb.outputs(res.wires()).unwrap();
@@ -68,10 +58,9 @@ pub fn psty_payload_sum(
         let mut ev = PsiEvaluator::new(&mut channel, rng.gen::<Block>()).unwrap();
 
         let res = ev
-            .circuit_psi_psty::<OpprfReceiver, _, _>(
+            .circuit_psi_psty_no_payloads::<OpprfReceiver, _, _>(
                 set_b,
-                Some(payload_b),
-                &mut fancy_payload_sum::<Ev, _>(),
+                &mut fancy_cardinality::<Ev, _>(),
             )
             .unwrap();
         let res_out = ev
@@ -84,13 +73,10 @@ pub fn psty_payload_sum(
 }
 
 pub fn main() {
-    let set_a: Vec<Vec<u8>> = (0..100u64).map(|el| el.to_le_bytes().to_vec()).collect();
+    let set_a: Vec<Vec<u8>> = (0..SET_SIZE).map(|el| el.to_le_bytes().to_vec()).collect();
     let mut set_b = set_a.clone();
     set_b[10] = (SET_SIZE + 1).to_le_bytes().to_vec();
 
-    let payload_a = int_vec_block512(vec![1u128; SET_SIZE], PAYLOAD_SIZE);
-    let payload_b = int_vec_block512(vec![1u128; SET_SIZE], PAYLOAD_SIZE);
-
-    let res = psty_payload_sum(&set_a, &set_b, &payload_a, &payload_b);
-    println!("Result is {} and should be {}", res, (SET_SIZE - 1) * 2);
+    let res = psty_cardinality(&set_a, &set_b);
+    println!("Result is {} and should be {}", res, (SET_SIZE - 1));
 }
