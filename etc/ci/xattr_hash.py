a3ae91af6eb1e432c136a9633f522f44baa115d1
Marc Rosen <marc@galois.com>
2024-06-11 22:07:09 -0400

    ci: extract stat_data into a new function

--- etc/ci/xattr_hash.py
+++ etc/ci/xattr_hash.py
@@ -2,11 +2,23 @@ import os
 import sys
 from hashlib import sha256
 from pathlib import Path
+from typing import List
 
 import cbor2
 from xattr import getxattr, setxattr  # type: ignore
 
-
+def _stat_data(path: Path) -> List[int]:
+    """Return a key which should change if path ever changes."""
+    # Based on https://apenwarr.ca/log/20181113
+    stat = path.stat()
+    return [
+        stat.st_mtime_ns,
+        stat.st_size,
+        stat.st_ino,
+        stat.st_mode,
+        stat.st_uid,
+        stat.st_gid,
+    ]
 
 def cached_hash(exe: Path) -> bytes:
     """
@@ -16,15 +28,7 @@ def cached_hash(exe: Path) -> bytes:
     hash. We use https://apenwarr.ca/log/20181113 to set the cache key for the
     hash.
     """
-    stat = exe.stat()
-    stat_data = [
-        stat.st_mtime,
-        stat.st_size,
-        stat.st_ino,
-        stat.st_mode,
-        stat.st_uid,
-        stat.st_gid,
-    ]
+    stat_data = _stat_data(exe)
     attr = "user.caching_test_runner_hash_cache"
     try:
         raw_data = getxattr(exe, attr)
