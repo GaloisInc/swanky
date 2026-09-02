627336ba47eb30b448339f136d579c9634de6965
Chris Phifer <cphifer@galois.com>
2026-09-01 17:34:32 -0700

    etc/readme.py: Apply formatting.

--- etc/readme.py
+++ etc/readme.py
@@ -60,10 +60,8 @@ def gen_crate_list(ctx: click.Context, check: bool) -> None:
         raise click.ClickException(
             "The following errors were encountered:\n" + "\n".join(errors)
         )
-    descriptions_md: Callable[[list[tuple[str, str]]], str] = (
-        lambda descriptions: "\n".join(
-            f"- **`{name}`**: {desc}" for name, desc in sorted(descriptions)
-        )
+    descriptions_md: Callable[[list[tuple[str, str]]], str] = lambda descriptions: (
+        "\n".join(f"- **`{name}`**: {desc}" for name, desc in sorted(descriptions))
     )
     description_body = _README_TEMPLATE.safe_substitute(
         core=descriptions_md(core_descriptions),
