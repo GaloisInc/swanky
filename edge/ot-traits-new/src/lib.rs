12bc67d4892a51f24149bd7031716d0513d85332
Chris Phifer <cphifer@galois.com>
2026-09-21 18:48:35 -0700

    ot-traits-new: Add RandomOT trait.
    This is based on the contents of
    https://gitlab-ext.galois.com/crypto/swanky/-/commit/05caf98f6730f448450f256eaff379651c65161b.
    
    Note that this trait returns the encryption keys in the outputs (for
    each bit on the sender side, for the selected bit on the receiver
    side).
    This is implemented such that ownership of `self` is taken, and a new
    owned structure is returned upon success (this means that the calling
    object can't incidentally be reused after running `random_ot`).

--- edge/ot-traits-new/src/lib.rs
+++ edge/ot-traits-new/src/lib.rs
@@ -3,7 +3,10 @@
 
 use swanky_channel::Channel;
 use swanky_error::Result;
+use swanky_field_binary::F2;
+use swanky_party::either::PartyEither;
 use swanky_rng::SwankyRng;
+use vectoreyes::U8x16;
 
 swanky_party::party_system! {
     pub mod party {
@@ -23,4 +26,27 @@ pub use party::*;
 pub trait OTInit<P: Party>: Sized {
     /// Initialize and return an OT protocol.
     fn init(rng: &mut SwankyRng, channel: &mut Channel) -> Result<Self>;
+}
+
+/// OT protocols where the messages are random.
+///
+/// 1-out-of-2 protocols can be constructed using protocols
+/// implementing this trait using a standard OTP construction.
+pub trait RandomOT<P: Party>: OTInit<P> {
+    /// Run random OT.
+    ///
+    /// `inputs` : For the sender, the number of input values.
+    ///            For the receiver, the selection bits.
+    /// `outputs`: For the sender, the pairs of encryption keys (as
+    ///            arrays).
+    ///            For the receiver, the selected encryption keys.
+    fn random_ot<I: IntoIterator<Item = F2>, O: Extend<PartyEither<P, [U8x16; 2], U8x16>>>(
+        self,
+        inputs: PartyEither<P, usize, I>,
+        outputs: &mut O,
+        rng: &mut SwankyRng,
+        channel: &mut Channel,
+    ) -> Result<Self>
+    where
+        I::IntoIter: ExactSizeIterator;
 }
