8def4daf5b98ac3fa63e424faab2598eccf804ed
Chris Phifer <cphifer@galois.com>
2026-09-21 17:55:45 -0700

    ot-traits-new: Add OTInit trait.
    This trait provides the `init` method for OT protocols, which produces
    a protocol object given a source of randomness and a channel over
    which to communicate.
    
    This is a separate trait because random OR and 'standard' OT protocols
    share this behavior (and, indeed, the latter can be built from the
    former); having this functionality live in its own trait reduces
    duplication and the possibility of errors.
    
    Traits added to encapsulate the behaviors of an OT protocol in the
    future will be built on top of this foundation.

--- edge/ot-traits-new/src/lib.rs
+++ edge/ot-traits-new/src/lib.rs
@@ -1,6 +1,10 @@
 #![deny(missing_docs)]
 //! Base traits for Oblivious Transfer protocols
 
+use swanky_channel::Channel;
+use swanky_error::Result;
+use swanky_rng::SwankyRng;
+
 swanky_party::party_system! {
     pub mod party {
         /// The sender in an OT protocol.
@@ -11,3 +15,12 @@ swanky_party::party_system! {
 }
 
 pub use party::*;
+
+/// Initialization of OT protocols.
+///
+/// Initialization is shared between random OT and constructions built
+/// on it, so this helps reduce duplication between implementations.
+pub trait OTInit<P: Party>: Sized {
+    /// Initialize and return an OT protocol.
+    fn init(rng: &mut SwankyRng, channel: &mut Channel) -> Result<Self>;
+}
