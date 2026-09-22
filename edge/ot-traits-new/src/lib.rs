9f8d1f3e836c5c60c1329de6e25efa21be18059c
Chris Phifer <cphifer@galois.com>
2026-09-21 17:18:35 -0700

    ot-traits-new: Add a party system for OT protocols.
    Defines the Sender and Receiver parties that communicate to perform
    OT.

--- edge/ot-traits-new/src/lib.rs
+++ edge/ot-traits-new/src/lib.rs
@@ -1,2 +1,13 @@
 #![deny(missing_docs)]
 //! Base traits for Oblivious Transfer protocols
+
+swanky_party::party_system! {
+    pub mod party {
+        /// The sender in an OT protocol.
+        Sender,
+        /// The receiver in an OT protocol.
+        Receiver,
+    }
+}
+
+pub use party::*;
