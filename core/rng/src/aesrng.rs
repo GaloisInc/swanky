73696a3bb6cb03eb224dd78cd16c2ce51d9252d6
Ben Hamlin <hamlinb@galois.com>
2026-09-23 15:35:16 -0700

    swanky-rng: Remove std::* refs in favor of core::*

--- core/rng/src/aesrng.rs
+++ core/rng/src/aesrng.rs
@@ -42,7 +42,7 @@ macro_rules! make_aes_rng {
         }
 
         impl core::fmt::Debug for $name {
-            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
                 f.debug_tuple(stringify!($name)).finish()
             }
         }
